41c113cfb43ccf202c45c63823df9e6d91ebe76e
NullDev (Chris) <shadow@nulldev.org>
2020-01-05 02:09:00 +0100

    added reaction handler

---
+++ src/handler/reactionHandler.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+// Utils
+let log = require("../utils/logger");
+
+const events = {
+    MESSAGE_REACTION_ADD: "messageReactionAdd",
+    MESSAGE_REACTION_REMOVE: "messageReactionRemove"
+};
+
+module.exports = async function(event, client){
+    if (!events.hasOwnProperty(event.t)) return;
+
+    const { d: data } = event;
+
+    const message = await client.channels.get(data.channel_id).fetchMessage(data.message_id);
+    const member = message.guild.members.get(client.users.get(data.user_id).id);
+
+    if (member.id !== client.user.id){
+        const role = message.guild.roles.find(r => r.name === message.content);
+        if (event.t === "MESSAGE_REACTION_ADD") member.addRole(role.id).catch(log.error);
+        else if (event.t === "MESSAGE_REACTION_REMOVE") member.removeRole(role.id).catch(log.error);
+    }
+};
