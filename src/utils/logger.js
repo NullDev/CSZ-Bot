a82e0e9147dde7ef5d5ddead555b6760da145fd5
NullDev (Chris) <shadow@nulldev.org>
2020-06-02 23:09:28 +0200

    added feature stacktrace to logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -22,9 +22,42 @@ let getDate = function(){
     return "[" + hour + ":" + min + ":" + sec + "]";
 };
 
+/**
+ * Get the StackTrace of the calee function
+ *
+ * @returns {string} StackTrace
+ */
+let getTrace = function(){
+    let err = new Error();
+
+    // Parse the whole stacktrace
+    let callerLine = err.stack.split("\n");
+
+    // Get everything after the third line
+    let splitArr = callerLine.filter((_, index) => index > 1);
+    let cleanArr = "";
+
+    for (let element of splitArr){
+        // We want to end the trace once we reach the stack comming from dependencies
+        if (element.match(/(node_modules)/gi)) break;
+
+        // If it isn't the first line, pad the string according to our logger structure
+        cleanArr += cleanArr.length ? "                 " : "";
+
+        // remove the "at xyz" string and terminate the line
+        cleanArr += element.replace(/(    at )/gi, "") + "\n";
+    }
+
+    // Remove last occourency of a new line character in order to continue logging seamlessly
+    return cleanArr.substring(0, cleanArr.lastIndexOf("\n")) + cleanArr.substring(cleanArr.lastIndexOf("\n") + 1);
+};
+
 module.exports = {
     error: function(input){
-        console.log(" \x1b[41m\x1b[30m x \x1b[0m\x1b[31m [ERROR] " + getDate() + " - " + input + "\x1b[0m");
+        console.log(
+            " \x1b[41m\x1b[315m x \x1b[0m\x1b[31m [ERROR] " + getDate() + " - " + input + "\n" +
+            "     StackTrace: " + getTrace() + "\x1b[0m"
+        );
     },
 
     warn: function(input){
