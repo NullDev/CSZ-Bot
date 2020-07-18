250cb269a0a5ea3aac988b7cdc0f172c25f01b82
NullDev (Chris) <shadow@nulldev.org>
2020-07-18 21:24:13 +0200

    added unban command

---
+++ src/commands/modcommands/unban.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+// Utils
+let config = require("../../utils/configHandler").getConfig();
+
+/**
+ * Unbans a given user
+ *
+ * @param {*} client
+ * @param {*} message
+ * @param {*} args
+ * @param {*} callback
+ * @returns {function} callback
+ */
+exports.run = (client, message, args, callback) => {
+    let mentioned = message.mentions?.users?.first?.();
+    
+    if (!mentioned) return callback(`Da ist kein username... Mach \`${config.bot_settings.prefix.command_prefix}unban \@username\``);
+
+    let mentionedUserObject = message.guild.member(mentioned);
+
+    if (mentionedUserObject.roles.some(r => r.name === config.ids.default_role)) return callback("Dieser User ist nicht gebannt du kek.");
+
+    let defaultRole = message.guild.roles.find(role => role.name === config.ids.default_role);
+    let bannedRole = message.guild.roles.find(role => role.name === config.ids.banned_role);
+
+    if (!defaultRole || !bannedRole) return callback("Eine der angegebenen Rollen für das bannen existiert nich.");
+
+    mentionedUserObject.addRole(defaultRole);
+    mentionedUserObject.removeRole(bannedRole);
+
+    message.channel.send(`User ${mentionedUserObject} wurde entbannt!`);
+
+    return callback();
+};
+
+exports.description = `Entbannt einen User indem er die ${config.ids.banned_role} Rolle wegnimmt.\nBenutzung: ${config.bot_settings.prefix.command_prefix}unban username`;
