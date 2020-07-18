d4f9baf0d24de6ecda7001713a698671d4d5aa61
NullDev (Chris) <shadow@nulldev.org>
2020-07-18 21:17:20 +0200

    Added ban command

---
+++ src/commands/modcommands/ban.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+// Utils
+let config = require("../../utils/configHandler").getConfig();
+
+/**
+ * Ban a given user
+ *
+ * @param {*} client
+ * @param {*} message
+ * @param {*} args
+ * @param {*} callback
+ * @returns {function} callback
+ */
+exports.run = (client, message, args, callback) => {
+    let mentioned = message.mentions?.users?.first?.();
+    let reason = args[1] ?? "Kein Grund angegeben.";
+    
+    if (!mentioned) return callback(`Da ist kein username... Mach \`${config.bot_settings.prefix.command_prefix}ban \@username [Banngrund]\``);
+
+    let mentionedUserObject = message.guild.member(mentioned);
+
+    if (mentionedUserObject.roles.some(r => r.name === config.ids.banned_role)) return callback("Dieser User ist bereits gebannt du kek.");
+
+    let defaultRole = message.guild.roles.find(role => role.name === config.ids.default_role);
+    let bannedRole = message.guild.roles.find(role => role.name === config.ids.banned_role);
+
+    if (!defaultRole || !bannedRole) return callback("Eine der angegebenen Rollen für das bannen existiert nich.");
+
+    mentionedUserObject.removeRole(defaultRole);
+    mentionedUserObject.addRole(bannedRole);
+
+    message.channel.send(`User ${mentionedUserObject} wurde gebannt!\nGrund: ${reason}`);
+
+    return callback();
+};
+
+exports.description = `Bannt einen User indem er die ${config.ids.banned_role} Rolle zuweist.\nBenutzung: ${config.bot_settings.prefix.command_prefix}ban username [Banngrund]`;
