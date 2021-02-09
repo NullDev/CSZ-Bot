3156b20e8894db08653254b624f22221c3775cc3
JennyRakete <rakete@sputnik>
2021-02-09 02:05:16 +0100

    * Mehrfachauswahl

--- src/handler/reactionHandler.js
+++ src/handler/reactionHandler.js
@@ -26,13 +26,34 @@ module.exports = async function(event, client){
 
     const message = await client.channels.cache.get(data.channel_id).messages.fetch(data.message_id);
 
-    if (message.author.id !== client.user.id || event.d.emoji.name !== "✅") return;
+    if (message.author.id !== client.user.id) return;
 
-    const member = message.guild.members.cache.get(client.users.cache.get(data.user_id).id);
+    if (event.d.emoji.name === "✅") {
+        const member = message.guild.members.cache.get(client.users.cache.get(data.user_id).id);
 
-    if (member.id !== client.user.id){
-        const role = message.guild.roles.cache.find(r => r.name === message.content);
-        if (event.t === "MESSAGE_REACTION_ADD") member.roles.add(role.id).catch(log.error);
-        else if (event.t === "MESSAGE_REACTION_REMOVE") member.roles.remove(role.id).catch(log.error);
+        if (member.id !== client.user.id) {
+            const role = message.guild.roles.cache.find(r => r.name === message.content);
+            if (event.t === "MESSAGE_REACTION_ADD") member.roles.add(role.id).catch(log.error);
+            else if (event.t === "MESSAGE_REACTION_REMOVE") member.roles.remove(role.id).catch(log.error);
+        }
+        return;
+    }
+
+    const numberEmojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"];
+    if (numberEmojis.includes(event.d.emoji.name) && event.t === "MESSAGE_REACTION_ADD") {
+        const member = message.guild.members.cache.get(client.users.cache.get(data.user_id).id);
+        if (member.id === client.user.id) return;
+        if (!message.embeds.length > 0) return;
+        if (!(message.embeds[0].author.name.indexOf("Umfrage") > -1)) return;
+        if (message.embeds[0].description.indexOf("Mehrfachauswahl") > -1) return;
+        const reactions = message.reactions.cache.filter(reaction => reaction.users.cache.has(member.id) && reaction._emoji.name !== event.d.emoji.name);
+        try {
+            for (const reaction of reactions.values()) {
+                await reaction.users.remove(member.id);
+            }
+        }
+        catch (e) {
+            console.error(e);
+        }
     }
 };
