c1634e0c2960d7bbb9f924c4388069586aa841c8
Jenny Rakete <rakete@rakete.xyz>
2021-02-10 20:15:42 +0100

    Mehrfachauswahl in Umfragen (#9)
    * added node dependence
    
    * fixed casing
    
    * * Mehrfachauswahl
    
    * 200iq
    
    * poll and strawpoll commands
    
    * only last answer to .vote is valid
    
    * fixed command name
    
    * refactoring
    
    Co-authored-by: Jenny Rakete <alan.dalisdadavis@gmail.com>
    Co-authored-by: JennyRakete <rakete@sputnik>
    Co-authored-by: NullDev (Chris) <shadow@nulldev.org>

--- src/handler/reactionHandler.js
+++ src/handler/reactionHandler.js
@@ -12,6 +12,8 @@ const events = {
     MESSAGE_REACTION_REMOVE: "messageReactionRemove"
 };
 
+const voteEmojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟", "👍", "👎"];
+
 /**
  * Handles changes on reactions
  *
@@ -26,13 +28,33 @@ module.exports = async function(event, client){
 
     const message = await client.channels.cache.get(data.channel_id).messages.fetch(data.message_id);
 
-    if (message.author.id !== client.user.id || event.d.emoji.name !== "✅") return;
+    if (message.author.id !== client.user.id) return;
 
-    const member = message.guild.members.cache.get(client.users.cache.get(data.user_id).id);
+    if (event.d.emoji.name === "✅"){
+        const member = message.guild.members.cache.get(client.users.cache.get(data.user_id).id);
 
-    if (member.id !== client.user.id){
-        const role = message.guild.roles.cache.find(r => r.name === message.content);
-        if (event.t === "MESSAGE_REACTION_ADD") member.roles.add(role.id).catch(log.error);
-        else if (event.t === "MESSAGE_REACTION_REMOVE") member.roles.remove(role.id).catch(log.error);
+        if (member.id !== client.user.id){
+            const role = message.guild.roles.cache.find(r => r.name === message.content);
+            if (event.t === "MESSAGE_REACTION_ADD") member.roles.add(role.id).catch(log.error);
+            else if (event.t === "MESSAGE_REACTION_REMOVE") member.roles.remove(role.id).catch(log.error);
+        }
+        return;
+    }
+
+    if (voteEmojis.includes(event.d.emoji.name) && event.t === "MESSAGE_REACTION_ADD"){
+        const member = message.guild.members.cache.get(client.users.cache.get(data.user_id).id);
+        if (
+            member.id === client.user.id ||
+            message.embeds.length !== 1 ||
+            message.embeds[0].author.name.indexOf("Strawpoll") < 0 && voteEmojis.slice(0, 10).includes(event.d.emoji.name)
+        ) return;
+
+        const reactions = message.reactions.cache.filter(reaction =>
+            reaction.users.cache.has(member.id) &&
+            reaction._emoji.name !== event.d.emoji.name &&
+            voteEmojis.includes(reaction._emoji.name)
+        );
+
+        for (let reaction of reactions.values()) await reaction.users.remove(member.id).catch(log.error);
     }
 };
