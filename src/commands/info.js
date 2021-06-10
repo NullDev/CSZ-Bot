8bef1a00af36a722e90405f6a54c6301e2a91166
NullDev <shadow@nulldev.org>
2021-06-10 14:04:21 +0200

    fetch contributors directly from GitHub

--- src/commands/info.js
+++ src/commands/info.js
@@ -4,6 +4,19 @@
 // = Copyright (c) NullDev = //
 // ========================= //
 
+let fetch = require("node-fetch").default;
+
+/**
+ * Get all contributors from GitHub
+ *
+ * @return {Promise<String>}
+ */
+let getContributors = function(){
+    return new Promise(async resolve => resolve((await (await fetch("https://api.github.com/repos/NullDev/CSC-Bot/contributors", {
+        headers: { Accept: "application/vnd.github.v3+json" }
+    })).json()).filter(e => e.type === "User").map(e => `<${e.html_url}> (Contributions: ${e.contributions})`).join("\n")));
+};
+
 /**
  * Shows some generic infos
  *
@@ -11,13 +24,14 @@
  * @param {import("discord.js").Message} message
  * @param {Array} args
  * @param {Function} callback
- * @returns {Function} callback
+ * @returns {Promise<Function>} callback
  */
-exports.run = (client, message, args, callback) => {
+exports.run = async(client, message, args, callback) => {
     message.react("✉");
     message.author.send(
-        "Programmiert von ShadowByte#1337 für die Coding Shitpost Zentrale (<https://discord.gg/FABdvae>)\n" +
-        "Contributions von Hans Lambda#8572, diewellenlaenge#5705, PoTTii#2582 und CreepSore#1337\n\n" +
+        "Programmiert von ShadowByte#1337 für die Coding Shitpost Zentrale (<https://discord.gg/FABdvae>)\n\n" +
+        "Contributions von:\n" +
+        (await getContributors()) + "\n\n" +
         "Eckdaten:\n" +
         "- Programmiersprache: NodeJS\n" +
         "- NodeJS Version: " + process.version + "\n" +
