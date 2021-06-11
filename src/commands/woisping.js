df848ef01330a151d4cc8229f80e8626c7f0964f
diewellenlaenge <diewellenlaenge@users.noreply.github.com>
2021-06-11 17:12:00 +0200

    Add user reaction treshold to .woisping
    Add moderators to use .woisping without being a Woisgang-member
    
    Change limit time in config to be seconds, not milliseconds

--- src/commands/woisping.js
+++ src/commands/woisping.js
@@ -8,6 +8,8 @@
 let log = require("../utils/logger");
 let config = require("../utils/configHandler").getConfig();
 
+const pendingMessagePrefix = "*(Pending-Woisgang-Ping, bitte zustimmen)*";
+
 // Internal storage, no need to save this persistent
 let lastPing = 0;
 
@@ -21,8 +23,10 @@ let lastPing = 0;
  * @param {Function} callback
  * @returns {Promise<Function>} callback
  */
-exports.run = (client, message, args, callback) => {
-    if (!message.member.roles.cache.has(config.ids.woisgang_role_id)){
+exports.run = async(client, message, args, callback) => {
+    const isMod = message.member.roles.cache.some(r => config.bot_settings.moderator_roles.includes(r.name));
+
+    if (!isMod && !message.member.roles.cache.has(config.ids.woisgang_role_id)){
         log.warn(`User "${message.author.tag}" (${message.author}) tried command "${config.bot_settings.prefix.command_prefix}woisping" and was denied`);
 
         return callback(
@@ -30,23 +34,86 @@ exports.run = (client, message, args, callback) => {
         );
     }
 
-    let woisgangRole = message.guild.roles.cache.get(config.ids.woisgang_role_id);
-
-    if (!woisgangRole) {
-        return callback("Iwas is totally falsch.");
-    }
-
-    const isMod = message.member.roles.cache.some(r => config.bot_settings.moderator_roles.includes(r.name));
-
     const now = Date.now();
 
-    if (!isMod && lastPing + config.bot_settings.woisping_limit > now) return callback("Piss dich und spam nicht.");
+    if (!isMod && lastPing + config.bot_settings.woisping_limit * 1000 > now) {
+        return callback("Piss dich und spam nicht.");
+    }
 
-    lastPing = now;
-    const reason = args.join(" ");
-    message.channel.send(`${woisgangRole} ${reason}`);
+    const reason = `${args.join(" ")} (von ${message.member})`;
+
+    if (isMod) {
+        lastPing = now;
+        message.channel.send(`<@&${config.ids.woisgang_role_id}> ${reason}`);
+    }
+    else {
+        const msg = await message.channel.send(`${pendingMessagePrefix} ${reason}`);
+        msg.react("👍");
+
+        // we don't set lastPing here to allow multiple concurrent requests
+        // let the most liked reason win...
+    }
 
     return callback();
 };
 
-exports.description = `Mitglieder der @Woisgang-Rolle können einen Ping an diese Gruppe absenden.\nUsage: ${config.bot_settings.prefix.command_prefix}woisping Text`;
+/**
+ * Handles changes on reactions specific to this command
+ *
+ * @param {any} event
+ * @param {import("discord.js").Client} client
+ * @param {import("discord.js").Message} message
+ * @returns
+ */
+exports.reactionHandler = async(event, client, message) => {
+    if (message.embeds.length !== 0
+		|| !message.content.startsWith(pendingMessagePrefix)
+		|| event.d.emoji.name !== "👍") {
+        return false;
+    }
+
+    const reaction = message.reactions.cache.get("👍");
+
+    // shouldn't happen
+    if (!reaction) {
+        return true;
+    }
+
+    const { d: data } = event;
+
+    const user = client.guilds.cache.get(config.ids.guild_id).members.cache.get(data.user_id);
+
+    if (!user) {
+        return true;
+    }
+
+    const isMod = user.roles.cache.some(r => config.bot_settings.moderator_roles.includes(r.name));
+
+    if (!isMod && !user.roles.cache.has(config.ids.woisgang_role_id)){
+        reaction.users.remove(data.user_id);
+        user.send("Somry, du bist leider kein Woisgang-Mitglied und darfst nicht abstimmen.");
+        return true;
+    }
+
+    const amount = reaction.count - 1;
+    const now = Date.now();
+    const couldPing = lastPing + config.bot_settings.woisping_limit * 1000 <= now;
+
+    if (isMod || (amount >= config.bot_settings.woisping_treshold && couldPing)) {
+        const reason = message.content.substr(pendingMessagePrefix.length + 1);
+
+        const {channel} = message;
+        await message.delete();
+
+        lastPing = now;
+        channel.send(`<@&${config.ids.woisgang_role_id}> ${reason}`);
+    }
+    else if (!couldPing) {
+        reaction.users.remove(data.user_id);
+        user.send("Somry, ich musste deine Zustimmung für den Woisgang-Ping entfernen, weil wir noch etwas warten müssen mit dem Ping.");
+    }
+
+    return true;
+};
+
+exports.description = `Mitglieder der @Woisgang-Rolle können einen Ping an diese Gruppe absenden. Es müssen mindestens ${config.bot_settings.woisping_treshold} Woisgang-Mitglieder per Reaction zustimmen.\nUsage: ${config.bot_settings.prefix.command_prefix}woisping Text`;
