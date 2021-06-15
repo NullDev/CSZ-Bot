fc853f0de8077381c189d3673e9ba365934d7763
NullDev <shadow@nulldev.org>
2021-06-15 16:21:07 +0200

    Added min age command

---
+++ src/commands/min.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+let config = require("../utils/configHandler").getConfig();
+
+/**
+ * Calculate a minimum moral age
+ *
+ * @param {import("discord.js").Client} client
+ * @param {import("discord.js").Message} message
+ * @param {Array} args
+ * @param {Function} callback
+ * @returns {Function} callback
+ */
+exports.run = (client, message, args, callback) => {
+    if (!args.length) return callback("Wie wärs wenn du auch ein Alter angibst?");
+    if (
+        isNaN(args[0])
+        || args[0] <= 0
+        || args[0] > Number.MAX_SAFE_INTEGER
+        || !Number.isInteger(Number(args[0]))
+    ) return callback("Das ist kein gültiger positiver 64Bit Integer...");
+
+    message.channel.send("Moralisch vertretbares Alter: " + ((Number(args[0]) / 2) + 7));
+
+    return callback();
+};
+
+exports.description = `Gibt dir die Moralisch vertretbare Altersgrenze für den Geschlechtsakt basierend auf deinem Alter zurück. \nUsage: ${config.bot_settings.prefix.command_prefix}min [dein Alter]`;
