f51b7e3c5bdc4559702339f48f99d844203ee8c0
Tobi <22715034+tobi6112@users.noreply.github.com>
2021-06-17 21:42:08 +0200

    Use template string instead of string concatening

--- src/commands/info.js
+++ src/commands/info.js
@@ -28,21 +28,24 @@ let getContributors = function(){
  */
 exports.run = async(client, message, args, callback) => {
     message.react("✉");
-    message.author.send(
-        "Programmiert von ShadowByte#1337 für die Coding Shitpost Zentrale (<https://discord.gg/FABdvae>)\n\n" +
-        "Contributions von:\n" +
-        (await getContributors()) + "\n\n" +
-        "Eckdaten:\n" +
-        "- Programmiersprache: NodeJS\n" +
-        "- NodeJS Version: " + process.version + "\n" +
-        "- PID: " + process.pid + "\n" +
-        "- Uptime (seconds): " + Math.floor(process.uptime()) + "\n" +
-        "- Platform: " + process.platform + "\n" +
-        "- System CPU usage time: " + process.cpuUsage().system + "\n" +
-        "- User CPU usage time: " + process.cpuUsage().user + "\n" +
-        "- Architecture: " + process.arch + "\n\n" +
-        "Source Code: <https://github.com/NullDev/CSC-Bot>"
-    );
+    message.author.send(`
+        Programmiert von ShadowByte#1337 für die Coding Shitpost Zentrale (<https://discord.gg/FABdvae>)
+        
+        Contributions von:
+        ${(await getContributors())} 
+        
+        Eckdaten:
+        - Programmiersprache: NodeJS
+        - NodeJS Version: ${process.version}
+        - PID: ${process.pid}
+        - Uptime (seconds): ${Math.floor(process.uptime())}
+        - Platform: ${process.platform}
+        - System CPU usage time: ${process.cpuUsage().system}
+        - User CPU usage time: ${process.cpuUsage().user}
+        - Architecture: ${process.arch}
+        
+        Source Code: <https://github.com/NullDev/CSC-Bot>
+    `.replace(/  +/g, "")); //Remove leading indents
 
     return callback();
 };
