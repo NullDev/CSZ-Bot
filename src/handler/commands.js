0b211632b9dc19b7cf4df915253361dc822b8e71
diewellenlaenge <diewellenlaenge@users.noreply.github.com>
2021-06-20 23:43:10 +0200

    Fix logic bug for permissions

--- src/handler/commands.js
+++ src/handler/commands.js
@@ -61,24 +61,28 @@ async function createApplicationCommands(client) {
     for (const [name, info] of this.allCommands) {
         // we are lazy and don't want to specify the command name twice in the module itself
         info.data.name = name;
-        info.data.defaultPermission = !info.isModCommand;
+
+        const permissions = info.permissions?.length > 0 ? info.permissions : [];
+
+        // always allow moderators to use commands, even if restricted in use for a specific role/user
+        if(permissions.length > 0 || info.isModCommand) {
+            permissions.push({
+                id: config.ids.moderator_role_id,
+                type: "ROLE",
+                permission: true
+            });
+        }
+
+        // defaultPermission says if the command is normally usable for everyone
+        // so if there are specific permissions, we have to disable it for everyone except those permitted
+        info.data.defaultPermission = (permissions.length === 0);
 
         client.application.commands.create(info.data, config.ids.guild_id)
             .then(cmdObject => {
                 log.info(`Successfully created application ${info.isModCommand ? "mod " : ""}command ${cmdObject.name} with ID ${cmdObject.id}`);
 
-                if(info.permissions && info.permissions.length > 0) {
-                    cmdObject.setPermissions(info.permissions, config.ids.guild_id);
-                }
-
-                if (info.isModCommand) {
-                    cmdObject.setPermissions([
-                        {
-                            id: config.ids.moderator_role_id,
-                            type: "ROLE",
-                            permission: true
-                        }
-                    ], config.ids.guild_id);
+                if (permissions.length > 0) {
+                    cmdObject.setPermissions(permissions, config.ids.guild_id);
                 }
             })
             .catch(err => log.error(err));
