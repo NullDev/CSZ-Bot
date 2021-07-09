eba9876b3efe6c0c0d11e247de9e7bc9b8f2e18e
Tobi <22715034+tobi6112@users.noreply.github.com>
2021-07-09 21:17:24 +0200

    Le fix au de regex

--- src/handler/messageHandler.js
+++ src/handler/messageHandler.js
@@ -59,7 +59,7 @@ module.exports = function(message, client){
 
     if (message.author.bot || nonBiased === "" || message.channel.type === "dm") return;
 
-    if (message.author.id === "348086189229735946" && (/(^(<:.+:\d+>|\s*)$)|^(\p{Emoji}|\s*)+$/gu).test(message.content.trim()/* .replace(/[\W_]+/g, "") */)) {
+    if (message.author.id === "348086189229735946" && (/^[\W\s_]*(<:.+:\d+>|\p{Emoji})[\W\s_]*$/gu).test(message.content.trim()/* .replace(/[\W_]+/g, "") */)) {
         message.delete();
     }
 
