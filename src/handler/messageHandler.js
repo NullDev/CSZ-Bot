151459b4076f36fe111fd6858ac28c5d9b6fd2f2
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-07-10 21:31:31 +0200

    Add where meme

--- src/handler/messageHandler.js
+++ src/handler/messageHandler.js
@@ -15,6 +15,10 @@ let config = require("../utils/configHandler").getConfig();
 // Handler
 let cmdHandler = require("./cmdHandler");
 
+let Jimp = require("jimp");
+let path = require("path");
+let fs = require("fs");
+
 /**
  * Performs inline reply to a message
  * @param {import("discord.js").Message} messageRef message to which should be replied
@@ -44,6 +48,29 @@ const getInlineReplies = function(messageRef, client) {
 };
 
 /**
+ * @param {string} text
+ * @returns {Promise<string>}
+ */
+const createWhereMeme = function(text) {
+    /** @type {import("jimp").Jimp} */
+    let image = null;
+    return Jimp.read({
+        url: "https://i.imgflip.com/52l6s0.jpg"
+    }).then(where => {
+        image = where;
+        return Jimp.loadFont("./assets/impact.fnt");
+    }).then(async font => {
+        const filename = `/tmp/where_meme_${Date.now()}.jpg`;
+        await image.print(font, 10, 10, {
+            text,
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+            alignmentY: Jimp.VERTICAL_ALIGN_TOP
+        }, image.bitmap.width).writeAsync(filename);
+        return filename;
+    });
+};
+
+/**
  * Handles incomming messages
  *
  * @param {Message} message
@@ -60,6 +87,24 @@ module.exports = function(message, client){
 
     if (message.author.id === "348086189229735946" && (/(^(<:.+:\d+>|\s*)$)|^(\p{Emoji}|\s*)+$/gu).test(message.content.trim())) {
         message.delete();
+    }
+
+    if ((/^wo(\s+\S+){1,3}\S$/gi).test(message.content.trim())) {
+        createWhereMeme(message.content.trim().replace(/\W+$/g, "").toUpperCase())
+            .then(where => {
+                message.channel.send({
+                    files: [{
+                        attachment: where,
+                        name: path.basename(where)
+                    }]
+                }).finally(() => fs.unlink(where, (err) => {
+                    if(err) {
+                        console.error(err);
+                    }
+                    return;
+                }));
+            })
+            .catch(err => console.error(err));
     }
 
     let isNormalCommand = message.content.startsWith(config.bot_settings.prefix.command_prefix);
