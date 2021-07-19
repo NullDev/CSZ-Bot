965fb53eaf86f0087e4ae32dcd44c8bac5d89dc4
diewellenlaenge <diewellenlaenge@users.noreply.github.com>
2021-07-19 16:45:24 +0200

    Migrate min command

---
+++ src/commands/min.ts
@@ -0,0 +1,31 @@
+"use strict";
+
+import { VerifiedCommandInteraction, Result, ApplicationCommandDefinition } from "../types";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+async function handler(interaction: VerifiedCommandInteraction): Promise<Result> {
+    const age = interaction.options.get("alter")?.value as number;
+
+    return { content: `Moralisch vertretbares Alter: ${((age / 2) + 7)}` };
+}
+
+export const applicationCommands: ApplicationCommandDefinition[] = [
+    {
+        handler,
+        data: {
+            name: "min",
+            description: "Gibt dir die Moralisch vertretbare Altersgrenze für den Geschlechtsakt basierend auf deinem Alter",
+            options: [
+                {
+                    name: "alter",
+                    description: "Dein Alter in Jahren",
+                    type: "INTEGER",
+                    required: true
+                }
+            ]
+        }
+    }
+];
