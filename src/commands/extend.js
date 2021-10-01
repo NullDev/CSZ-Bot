c0728157117a6af3067f348bb35560c008874ef9
holzmaster <holzmaster@outlook.com>
2021-10-02 01:03:07 +0200

    Migrate extend

--- src/commands/extend.js
+++ src/commands/extend.js
@@ -1,6 +1,6 @@
-// =========================================== //
+// ==================================================== //
 // = Copyright (c) NullDev & diewellenlaenge & nimbl0 = //
-// =========================================== //
+// ==================================================== //
 
 /**
  * @typedef {import("discord.js").TextChannel} TC
@@ -18,16 +18,15 @@ const config = getConfig();
  * @param {import("discord.js").Client} client
  * @param {import("discord.js").Message} message
  * @param {Array} args
- * @param {Function} callback
- * @returns {Promise<Function>} callback
+ * @returns {Promise<string | void>}
  */
-export const run = async(client, message, args, callback) => {
-    if (!message.reference) return callback("Bruder schon mal was von der Replyfunktion gehört?");
-    if (message.reference.guildID !== config.ids.guild_id || !message.reference.channelID) return callback("Bruder bleib mal hier auf'm Server.");
+export const run = async(client, message, args) => {
+    if (!message.reference) return "Bruder schon mal was von der Replyfunktion gehört?";
+    if (message.reference.guildID !== config.ids.guild_id || !message.reference.channelID) return "Bruder bleib mal hier auf'm Server.";
 
     let channel = client.guilds.cache.get(config.ids.guild_id).channels.cache.get(message.reference.channelID);
 
-    if (!channel) return callback("Bruder der Channel existiert nicht? LOLWUT");
+    if (!channel) return "Bruder der Channel existiert nicht? LOLWUT";
 
     let replyMessage = null;
 
@@ -36,28 +35,28 @@ export const run = async(client, message, args, callback) => {
     }
     catch (err) {
         log.error(err);
-        return callback("Bruder irgendwas stimmt nicht mit deinem Reply ¯\_(ツ)_/¯");
+        return "Bruder irgendwas stimmt nicht mit deinem Reply ¯\_(ツ)_/¯";
     }
 
-    if (replyMessage.author.id !== client.user.id || replyMessage.embeds.length !== 1) return callback("Bruder das ist keine Umfrage ಠ╭╮ಠ");
-    if (!replyMessage.embeds[0].author.name.startsWith("Umfrage") && !replyMessage.embeds[0].author.name.startsWith("Strawpoll")) return callback("Bruder das ist keine Umfrage ಠ╭╮ಠ");
-    if (!replyMessage.editable) return callback("Bruder aus irgrndeinem Grund hat der Bot verkackt und kann die Umfrage nicht bearbeiten :<");
-    if (replyMessage.embeds[0].color !== 3066993) return callback("Bruder die Umfrage ist nicht erweiterbar (ง'̀-'́)ง");
+    if (replyMessage.author.id !== client.user.id || replyMessage.embeds.length !== 1) return "Bruder das ist keine Umfrage ಠ╭╮ಠ";
+    if (!replyMessage.embeds[0].author.name.startsWith("Umfrage") && !replyMessage.embeds[0].author.name.startsWith("Strawpoll")) return "Bruder das ist keine Umfrage ಠ╭╮ಠ";
+    if (!replyMessage.editable) return "Bruder aus irgrndeinem Grund hat der Bot verkackt und kann die Umfrage nicht bearbeiten :<";
+    if (replyMessage.embeds[0].color !== 3066993) return "Bruder die Umfrage ist nicht erweiterbar (ง'̀-'́)ง";
 
     let oldPollOptions = replyMessage.embeds[0].description.split("\n");
 
-    if (oldPollOptions.length === poll.OPTION_LIMIT) return callback("Bruder die Umfrage ist leider schon voll (⚆ ͜ʖ⚆)");
+    if (oldPollOptions.length === poll.OPTION_LIMIT) return "Bruder die Umfrage ist leider schon voll (⚆ ͜ʖ⚆)";
 
     let oldPollOptionsLength = replyMessage.embeds[0].description.length;
-    if (oldPollOptionsLength > poll.TEXT_LIMIT) return callback("Bruder die Umfrage ist leider schon voll (⚆ ͜ʖ⚆)");
+    if (oldPollOptionsLength > poll.TEXT_LIMIT) return "Bruder die Umfrage ist leider schon voll (⚆ ͜ʖ⚆)";
 
     for (let i = 0; i < oldPollOptions.length; ++i) {
         if (!oldPollOptions[i].startsWith(poll.LETTERS[i])) {
-            return callback("Bruder das ist keine Umfrage ಠ╭╮ಠ");
+            return "Bruder das ist keine Umfrage ಠ╭╮ಠ";
         }
     }
 
-    if (!args.length) return callback("Bruder da sind keine Antwortmöglichkeiten :c");
+    if (!args.length) return "Bruder da sind keine Antwortmöglichkeiten :c";
 
     let additionalPollOptions = args.join(" ").split(";").map(e => e.trim()).filter(e => e.replace(/\s/g, "") !== "");
     let additionalPollOptionsLength = 0;
@@ -65,9 +64,9 @@ export const run = async(client, message, args, callback) => {
         additionalPollOptionsLength += additionalPollOption.length;
     }
 
-    if (!additionalPollOptions.length) return callback("Bruder da sind keine Antwortmöglichkeiten :c");
-    if(oldPollOptionsLength + additionalPollOptionsLength > poll.TEXT_LIMIT) return callback("Bruder die Umfrage ist zu lang");
-    if(oldPollOptions.length + additionalPollOptions.length > poll.OPTION_LIMIT) return callback(`Bruder mit deinen Antwortmöglichkeiten wird das Limit von ${poll.OPTION_LIMIT} überschritten!`);
+    if (!additionalPollOptions.length) return "Bruder da sind keine Antwortmöglichkeiten :c";
+    if(oldPollOptionsLength + additionalPollOptionsLength > poll.TEXT_LIMIT) return "Bruder die Umfrage ist zu lang";
+    if(oldPollOptions.length + additionalPollOptions.length > poll.OPTION_LIMIT) return `Bruder mit deinen Antwortmöglichkeiten wird das Limit von ${poll.OPTION_LIMIT} überschritten!`;
 
     let originalAuthor = replyMessage.embeds[0].author.name.split(" ")[2];
     let authorNote = originalAuthor !== message.author.username ? ` (von ${message.author.username})` : "";
@@ -81,11 +80,11 @@ export const run = async(client, message, args, callback) => {
         delete embed.footer;
     }
 
-    replyMessage.edit(undefined, embed).then(async msg => {
-        for (let i in additionalPollOptions) await msg.react(poll.EMOJI[oldPollOptions.length + Number(i)]);
-    }).then(() => message.delete());
-
-    return callback();
+    const msg = await replyMessage.edit(undefined, embed);
+    for (let i in additionalPollOptions) {
+        await msg.react(poll.EMOJI[oldPollOptions.length + Number(i)]);
+    }
+    await message.delete();
 };
 
 export const description = `Nutzbar als Reply auf eine mit --extendable erstellte Umfrage, um eine/mehrere Antwortmöglichkeit/en hinzuzüfgen. Die Anzahl der bestehenden und neuen Antwortmöglichkeiten darf 10 nicht übersteigen.\nUsage: ${config.bot_settings.prefix.command_prefix}extend [Antwort 1] ; [...]`;
