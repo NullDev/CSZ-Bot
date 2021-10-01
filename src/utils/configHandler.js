57076472a77758bd9d2ff1e3251d57c0d57583a5
holzmaster <holzmaster@outlook.com>
2021-10-01 22:08:55 +0200

    Upgrade utils to ESM

--- src/utils/configHandler.js
+++ src/utils/configHandler.js
@@ -1,18 +1,14 @@
-"use strict";
-
 // ========================= //
 // = Copyright (c) NullDev = //
 // ========================= //
 
-// Core Modules
-let fs = require("fs");
-let path = require("path");
+import * as fs from "fs";
+import * as path from "path";
 
-// Utils
-let log = require("./logger");
+import * as log from "../utils/logger";
 
-const packagefile = require(path.resolve("package.json"));
-const configPath  = path.resolve("config.json");
+const packagefile = require(path.resolve("package.json")); // TODO: Check if this is till working
+const configPath = path.resolve("config.json");
 
 /**
  * Check if the config is valid JSON
@@ -20,11 +16,11 @@ const configPath  = path.resolve("config.json");
  * @param {*} obj
  * @returns {Boolean} whether it is valid JSON
  */
-let validJson = function(obj){
+let isValidJson = function (obj) {
     try {
         JSON.parse(obj);
     }
-    catch (e){
+    catch (e) {
         return false;
     }
     return true;
@@ -35,8 +31,8 @@ let validJson = function(obj){
  *
  * @returns {Object} JSON Content
  */
-let getConfig = function(){
-    if (!fs.existsSync(configPath)){
+export const getConfig = function () {
+    if (!fs.existsSync(configPath)) {
         log.error("Config does not exist! Make sure you copy config.template.json and paste it as 'config.json'. Then configure it.");
         process.exit(1);
     }
@@ -45,37 +41,30 @@ let getConfig = function(){
     try {
         jsondata = String(fs.readFileSync(configPath));
     }
-    catch (e){
+    catch (e) {
         log.error(`Cannot read config file: ${e}`);
         process.exit(1);
     }
 
-    if (validJson(jsondata)) return JSON.parse(jsondata);
+    if (isValidJson(jsondata)) return JSON.parse(jsondata);
 
     log.error("Config is not valid JSON. Stopping...");
     return process.exit(1);
 };
 
-let getVersion = function(){
+export const getVersion = function () {
     return packagefile.version;
 };
 
-let getName = function(){
+export const getName = function () {
     return packagefile.name;
 };
 
-let getAuthor = function(){
+export const getAuthor = function () {
     return packagefile.author;
 };
 
-let getDescription = function(){
+export const getDescription = function () {
     return packagefile.description;
 };
 
-module.exports = {
-    getConfig,
-    getVersion,
-    getName,
-    getAuthor,
-    getDescription
-};
