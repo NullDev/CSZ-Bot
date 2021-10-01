294c684f1117d2ada672f030d50bdf59d19de127
janmer\janmer <17313367+JanMer97@users.noreply.github.com>
2021-10-01 12:18:47 +0200

    Litte inspirationen von inspirobot.me #75

---
+++ src/commands/erleuchtung.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// ================================= //
+// = Copyright (c) Ehrenvio der G = //
+// =============================== //
+
+// Utils
+let config = require("../utils/configHandler").getConfig();
+const fetch = require("node-fetch").default;
+const moment = require("moment");
+
+const INSPIRATION_GENERATEAPI_URL = "https://inspirobot.me/api?generate=true";
+
+
+async function getInspiration() {
+    const promptResponse = await fetch(INSPIRATION_GENERATEAPI_URL, {
+        method: "GET"
+    });
+    return await promptResponse.text();
+}
+
+/**
+ * Sends a generated inspirational quote from inspirobot
+ *
+ * @param {import("discord.js").Client} _client
+ * @param {import("discord.js").Message} message
+ * @param {Array} args
+ * @param {Function} callback
+ * @returns {Function} callback
+ */
+exports.run = (_client, message, args, callback) => {
+    getInspiration()
+        .then(response => {
+            const envelope = {
+                embed: {
+                    image: {
+                        url: response
+                    },
+                    timestamp: moment.utc().format(),
+                    author: {
+                        name: `${message.author.username} wurde erleuchtet`,
+                        icon_url: message.author.displayAvatarURL()
+                    },
+                    footer: {
+                        text: `🙏 Glaub an dich 🙏`
+                    }
+                }
+            };
+
+            return message.channel
+                .send(envelope)
+                .then(sentMessage => {
+                    return Promise.all([
+                        message.delete()
+                    ]);
+                });
+        })
+        .then(() => callback())
+        .catch(error => {
+            callback(error);
+        });
+};
+
+exports.description = `Gönnt dir eine zufällige Erleuchtung.\nBenutzung: ${config.bot_settings.prefix.command_prefix}erleuchtung`;
