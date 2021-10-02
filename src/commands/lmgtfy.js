52f602357eec7c04e8166b6237f24977bd6a246a
Marcus Kram <m.kram@live.at>
2021-10-02 11:09:44 +0200

    Bugfixes for command - check if quotes message contains text and add some docs.

--- src/commands/lmgtfy.js
+++ src/commands/lmgtfy.js
@@ -3,12 +3,28 @@
 // Utils
 let config = require("../utils/configHandler").getConfig();
 
+/* Base URL of Search Engine */
 const GOOGLE_URL = "https://letmegooglethat.com/?q=";
 
+/**
+ * Basically all we need is to combine our Base URL and URI Encode the query, Website does the rest for us.
+ *
+ * @param {string} msg
+ * @returns {string}
+ */
 const buildSearchQuery = function(msg) {
     return GOOGLE_URL + encodeURIComponent(msg);
 };
 
+/**
+ * Search for a given text
+ *
+ * @param {import("discord.js").Client} client
+ * @param {import("discord.js").Message} message
+ * @param {Array} args
+ * @param {Function} callback
+ * @returns {Function} callback
+ */
 exports.run = async(client, message, args, callback) => {
     const messageRef = message.reference?.messageID;
     const text = message.content.slice(`${config.bot_settings.prefix.command_prefix}lmgtfy `.length);
@@ -19,9 +35,15 @@ exports.run = async(client, message, args, callback) => {
     // If reply to message
     if (messageRef) {
         const quotedMessage = await message.channel.messages.fetch(messageRef);
-        reply = `Hey, ${quotedMessage.author}, `;
-        if(!text) {
-            reply += `du hättest auch googeln können: ${buildSearchQuery(quotedMessage.content)}, danke dir ${message.author}`;
+        if (!!quotedMessage.content) {
+            reply = `Hey, ${quotedMessage.author}, `;
+            if (!text) {
+                reply += `du hättest auch googeln können: ${buildSearchQuery(quotedMessage.content)}, danke dir ${message.author}`;
+            }
+        }
+        else if (!text) {
+            message.channel.send("Bruder da ist nichts wonach ich suchen kann").then(() => message.delete());
+            return callback;
         }
     }
 
