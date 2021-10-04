3999d5b7627c5e0c2543ba4059377bc32f4e19ea
Tobias Hund <tobias.hund@innovation-hub.de>
2021-10-04 22:10:46 +0200

    Add Type safety

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,7 +1,7 @@
-import { Client } from "discord.js";
 import { Command } from "./command";
 import { SlashCommandBuilder } from '@discordjs/builders';
 import fetch from "node-fetch";
+import { Client, Message } from "discord.js";
 
 export class InfoCommand implements Command {
     public get applicationCommand(): SlashCommandBuilder {
@@ -11,33 +11,26 @@ export class InfoCommand implements Command {
     }
 }
 
-const fetchContributions = () => {
-    return fetch("https://api.github.com/repos/NullDev/CSC-Bot/contributors", {
-        headers: { Accept: "application/vnd.github.v3+json" }
-    }).then(res => res.json());
+interface Contributors {
+    type: string,
+    html_url: string,
+    contributions: number
 };
 
-/**
- * Get all contributors from GitHub
- *
- * @return {Promise<String>}
- */
-let formatContributors = (contributors) => {
+const fetchContributions = (): Promise<Array<Contributors>> => {
+    return fetch("https://api.github.com/repos/NullDev/CSC-Bot/contributors", {
+        headers: { Accept: "application/vnd.github.v3+json" }
+    }).then((res : any) => res.json());
+};
+
+let formatContributors = (contributors: Array<Contributors>): string => {
     return contributors
         .filter(e => e.type === "User")
         .map(e => `<${e.html_url}> (Contributions: ${e.contributions})`)
         .join("\n");
 };
 
-/**
- * Shows some generic infos
- *
- * @param {import("discord.js").Client} client
- * @param {import("discord.js").Message} message
- * @param {Array<unknown>} args
- * @returns {Promise<string | void>}
- */
-export const run = async(client, message, args) => {
+export const run = async(_client : Client, message: Message, args: Array<unknown>) => {
     const contributors = await fetchContributions();
     const formattedContributors = formatContributors(contributors);
 
