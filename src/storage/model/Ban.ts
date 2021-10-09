eef7b9b798668cee286850e84b9b8caa1ed8a56d
holzmaster <holzmaster@outlook.com>
2021-10-10 01:43:29 +0200

    Add more fields to model

--- src/storage/model/Ban.ts
+++ src/storage/model/Ban.ts
@@ -4,7 +4,9 @@ import { v4 as uuidv4 } from "uuid";
 export interface BanAttributes {
     id: string;
     userId: string;
+    reason: string | null;
     bannedUntil: number;
+    isSelfBan: boolean;
 }
 
 export interface BanCreationAttributes extends Optional<BanAttributes, "id"> { }
@@ -12,7 +14,9 @@ export interface BanCreationAttributes extends Optional<BanAttributes, "id"> { }
 export default class Ban extends Model<BanAttributes, BanCreationAttributes> implements BanAttributes {
     id!: string;
     userId!: string;
+    reason!: string | null;
     bannedUntil!: number;
+    isSelfBan!: boolean;
 
     readonly createdAt!: Date;
     readonly updatedAt!: Date;
@@ -28,16 +32,33 @@ export default class Ban extends Model<BanAttributes, BanCreationAttributes> imp
                 type: DataTypes.STRING(32),
                 allowNull: false
             },
+            reason: {
+                type: DataTypes.STRING(255),
+                allowNull: true
+            },
             bannedUntil: {
-                type: DataTypes.DATE,
+                type: DataTypes.BIGINT(), // 64 bit integer, used for unix timestamps
                 allowNull: false,
             },
+            isSelfBan: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+            }
         }, {
             sequelize,
             indexes: [
                 {
                     unique: true,
-                    fields: ["bannedUntil"]
+                    fields: ["userId"]
+                },
+                {
+                    using: "BTREE",
+                    fields: [
+                        {
+                            name: "bannedUntil",
+                            order: "ASC",
+                        }
+                    ]
                 }
             ]
         });
