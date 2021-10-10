c953b50ef479d4d2d4266c021a419a193a0722d9
holzmaster <holzmaster@outlook.com>
2021-10-10 16:55:59 +0200

    Add some types

--- src/commands/modcommands/ban.js
+++ src/commands/modcommands/ban.js
@@ -9,6 +9,10 @@ const config = getConfig();
 
 // #region Banned User Role Assignment
 
+/**
+ * @param {import("discord.js").User} user
+ * @returns {boolean} true if successful
+ */
 const assignBannedRoles = (user) => {
     let defaultRole = user.guild.roles.cache.find(role => role.id === config.ids.default_role_id);
     let bannedRole = user.guild.roles.cache.find(role => role.id === config.ids.banned_role_id);
@@ -30,8 +34,14 @@ const assignBannedRoles = (user) => {
         user.roles.remove(user.guild.roles.cache.find(role => role.id === config.ids.trusted_role_id));
         user.roles.add(user.guild.roles.cache.find(role => role.id === config.ids.trusted_banned_role_id));
     }
+
+    return true;
 };
 
+/**
+ * @param {import("discord.js").User} user
+ * @returns {boolean} true if successful
+ */
 export const restoreRoles = (user) => {
     let defaultRole = user.guild.roles.cache.find(role => role.id === config.ids.default_role_id);
     let bannedRole = user.guild.roles.cache.find(role => role.id === config.ids.banned_role_id);
@@ -60,6 +70,13 @@ export const restoreRoles = (user) => {
 // #endregion
 
 
+/**
+ * @param {import("discord.js").User} user
+ * @param {string} reason
+ * @param {boolean} isSelfBan
+ * @param {number | undefined} durationInHours
+ * @returns
+ */
 export const ban = async(user, reason, isSelfBan, durationInHours) => {
     await assignBannedRoles(user);
 
@@ -84,11 +101,14 @@ export const run = async(client, message, args) => {
     if (!mentioned) return `Da ist kein username... Mach \`${config.bot_settings.prefix.mod_prefix}ban \@username [Banngrund]\``;
 
     let mentionedUser = message.guild.members.cache.get(mentioned.id);
+
+    if (mentionedUser === undefined) return `Konnte User <@!${mentioned.id}> nicht finden.`;
+
     if (mentionedUser.id === "371724846205239326" || mentionedUser.id === client.user.id) return "Fick dich bitte.";
 
     const existingBan = await Ban.findExisting(mentionedUser);
     if (existingBan !== null) {
-        if (mentionedUser.roles.cache.some(r => r.id === config.ids.banned_role_id)) {return "Dieser User ist bereits gebannt du kek.";}
+        if (mentionedUser.roles.cache.some(r => r.id === config.ids.banned_role_id)) { return "Dieser User ist bereits gebannt du kek."; }
 
         return "Dieser nutzer ist laut Datenbank gebannt, ihm fehlt aber die Rolle. Fix das.";
     }
@@ -104,6 +124,10 @@ Falls du Fragen zu dem Bann hast, kannst du dich im <#${config.ids.banned_channe
 Lg & xD™`
     );
 };
+
+/**
+ * @param {import("discord.js").Client} client
+ */
 export const startCron = (client) => {
     cron.schedule("* * * * *", async() => {
         const now = Date.now();
@@ -114,8 +138,8 @@ export const startCron = (client) => {
             for (const expiredBan of expiredBans) {
                 await expiredBan.destroy();
 
-                const user = client.guilds.cache.get(config.ids.guild_id).members.cache.get(expiredBan.userId);
-                if (!user) {continue;}
+                const user = client.guilds.cache.get(config.ids.guild_id)?.members.cache.get(expiredBan.userId);
+                if (!user) continue;
 
                 restoreRoles(user);
 
