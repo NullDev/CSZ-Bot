2d60f9173903892e769c13cb96703c732f5b8c67
LolRiTTeR <1516296+LolRiTTeR@users.noreply.github.com>
2021-11-02 13:58:37 +0100

    Added new command: penis

---
+++ src/commands/penis.ts
@@ -0,0 +1,20 @@
+// @ts-ignore
+import { Client, Message } from "discord.js";
+import { MessageCommand } from "./command";
+
+/**
+ * Penis command. Displays the users penis length
+ */
+export class PenisCommand implements MessageCommand {
+    modCommand: boolean = false;
+    name = "penis";
+    description = "Zeigt dir die Schwanzlänge eines Nutzers an.";
+
+    /**
+     * Replies to the message with a random penis length
+     */
+    async handleMessage(message: Message, client: Client): Promise<unknown> {
+        const penis = "=".repeat(Math.floor(Math.random() * 30));
+        return message.reply(`8${penis}D`);
+    }
+}
