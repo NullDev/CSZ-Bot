fa880773219eb9eb16633e723443c1198c3d408b
LolRiTTeR <1516296+LolRiTTeR@users.noreply.github.com>
2021-11-03 17:51:18 +0100

    Implemented new special command: bonk

---
+++ src/commands/special/bonk.ts
@@ -0,0 +1,64 @@
+import { Message, Client, User } from "discord.js";
+import Jimp from "jimp";
+import path from "path";
+import { SpecialCommand } from "../command";
+import * as fs from "fs";
+import * as log from "../../utils/logger";
+
+const createBonkMeme = async(author: any): Promise<string> => {
+    const image = await Jimp.read("https://i.imgur.com/nav6WWX.png");
+    const filename = `/tmp/bonk_meme_${Date.now()}.png`;
+    const avatarURL = author.avatarURL({ format: "png" });
+    let avatar = await Jimp.read(avatarURL);
+
+    avatar = avatar.resize(128, 128);
+
+    await image.composite(avatar, 120, 90, {
+        mode: Jimp.BLEND_DESTINATION_OVER,
+        opacitySource: 1,
+        opacityDest: 1
+    }).writeAsync(filename);
+
+    return filename;
+};
+
+export class BonkCommand implements SpecialCommand {
+    name: string = "Bonk";
+    description: string = "Bonkt einen User";
+    pattern: RegExp = /^bonk(.*)$/i;
+    randomness = 1;
+
+    async handleSpecialMessage(message: Message, client: Client<boolean>): Promise<unknown> {
+        const messageRef = message.reference?.messageId;
+        const messagePing = message.mentions?.users.first();
+        let toBeBonked;
+
+        // If reply to message
+        if(messageRef) {
+            const msg = await message.channel.messages.fetch(messageRef);
+            toBeBonked = msg.author;
+        } // If a user is mentioned in the message
+        else if (messagePing) {
+            toBeBonked = message.mentions.users.first();
+        } // If nothing from above applies, use the command sender
+        else {
+            toBeBonked = message.author;
+        }
+        const meme = await createBonkMeme(toBeBonked);
+
+        try {
+            await message.channel.send({
+                files: [{
+                    attachment: meme,
+                    name: path.basename(meme)
+                }]
+            });
+        }
+        catch(err) {
+            log.error(`Could not create where meme: ${err}`);
+        }
+        finally {
+            return await fs.promises.unlink(meme);
+        }
+    }
+}
