bd4eacac2997171e656d5451dbf317f317aa3dfa
holzmaster <holzmaster@outlook.com>
2021-11-29 00:14:13 +0100

    Make graph prettier

--- src/commands/stempelgraph.ts
+++ src/commands/stempelgraph.ts
@@ -1,49 +1,118 @@
-import { Client, ClientUser, GuildMember, Message, Snowflake, User } from "discord.js";
-import { svg2png } from "svg-png-converter";
-
 import Viz from "viz.js";
 import { Module, render } from "viz.js/full.render.js";
-
-const viz = new Viz({ Module, render });
+import { GuildMember, Message, Snowflake } from "discord.js";
+import { svg2png } from "svg-png-converter";
 
 import { getConfig } from "../utils/configHandler";
 import type { CommandFunction } from "../types";
 import Stempel from "../storage/model/Stempel";
 import * as log from "../utils/logger";
+import { isMod } from "../utils/userUtils";
 
 const config = getConfig();
+
+const viz = new Viz({ Module, render });
 
 interface StempelConnection {
     inviter: GuildMember;
     invitee: GuildMember;
 }
 
-type RoleInGraph = "woisgang" | "trusted" | "gruendervaeter" | "moderator" | "administrator";
+type RoleInGraph =
+    | "woisgang"
+    | "trusted"
+    | "moderator"
+    | "gruendervaeter"
+    | "administrator";
+
+const roleColors: Partial<Record<RoleInGraph, string>> = {
+    // Order is important here: The lower the color, the higher the priority
+    trusted: "#e91e63",
+    moderator: "#5865f2",
+    gruendervaeter: "#faa81a",
+    administrator: "#8b51ff"
+};
 
 interface UserInfo {
+    member: GuildMember;
     name: string;
-    groups: readonly RoleInGraph[];
+    roles: readonly RoleInGraph[];
+}
+
+function getMemberNode(member: UserInfo): string {
+    const { roles } = member;
+    let label = "";
+
+    if (roles.includes("woisgang")) {
+        label += "🎤 ";
+    }
+    label += member.name;
+    if (roles.includes("trusted")) {
+        label += " ✅";
+    }
+
+    let boxColor = "#ffffff";
+    for (const [roleName, color] of Object.entries(roleColors)) {
+        if (!roleName || !color) {
+            continue;
+        }
+
+        if (roles.includes(roleName as RoleInGraph)) {
+            boxColor = color;
+        }
+    }
+
+    const escapedLabel = label.replaceAll('"', '\\"'); // dirty hack to fix quotes in user names
+    return `"${member.member.id}" [label="${escapedLabel}", color="${boxColor}"]`;
 }
 
 async function drawStempelgraph(stempels: StempelConnection[], userInfo: Map<GuildMember, UserInfo>): Promise<Buffer> {
     const layoutEngine = "dot";
 
-    const nodeStyles = "";
+    const inviterNodes = stempels
+        .map(s => userInfo.get(s.inviter)!)
+        .map(getMemberNode)
+        .join(";\n");
+
+    const inviteeNodes = stempels
+        .map(s => userInfo.get(s.invitee)!)
+        .map(getMemberNode)
+        .join(";\n");
 
     const connections = stempels
-        .map(s => `"${userInfo.get(s.inviter)!.name}" -> "${userInfo.get(s.invitee)!.name}"`)
+        .map(s => `"${s.inviter.id}" -> "${s.invitee.id}"`)
         .join(";\n");
 
     const dotSrc = `digraph {
         layout = ${layoutEngine};
-        node [shape=box];
-        ${nodeStyles}
+        splines=line;
+
+        bgcolor="#36393f";
+        fontcolor="#ffffff";
+        fontname="Monospace"
+        label="CSZ Social Graph";
+
+        node [
+            color="#ffffff"
+            fontcolor="#ffffff",
+            labelfontcolor="#ffffff",
+            shape=box,
+        ];
+
+        edge [
+            color="#ffffff"
+            fontcolor="#ffffff",
+            labelfontcolor="#ffffff",
+        ];
+
+        ${inviterNodes}
+        ${inviteeNodes}
         ${connections}
     }`;
 
-    const srvStr = await viz.renderString(dotSrc);
+    const svgSrc = await viz.renderString(dotSrc);
     return await svg2png({
-        input: srvStr,
+        input: svgSrc,
         encoding: "buffer",
         format: "png"
     });
@@ -75,9 +144,9 @@ function getRoles(member: GuildMember): RoleInGraph[] {
     if (member.roles.cache.has(config.ids.gruendervaeter_role_id)) {
         res.push("gruendervaeter");
     }
-    // if (member.roles.cache.has(config.bot_settings.moderator_id)) {
-    //     res.push("moderator");
-    // }
+    if (isMod(member)) {
+        res.push("moderator");
+    }
     // if (member.roles.cache.has(config.bot_settings.administrator_id)) {
     //     res.push("administrator");
     // }
@@ -107,8 +176,9 @@ export const run: CommandFunction = async (client, message, args) => {
     const graphUserInfo = new Map<GuildMember, UserInfo>();
     for (const member of memberInfoMap.values()) {
         graphUserInfo.set(member, {
+            member,
             name: member.nickname ?? member.displayName,
-            groups: getRoles(member)
+            roles: getRoles(member)
         });
     }
 
