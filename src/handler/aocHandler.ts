08039b67bb6d9eb8429ee3dc63b324702d7964ad
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-12-13 11:11:14 +0100

    add debugging messages

--- src/handler/aocHandler.ts
+++ src/handler/aocHandler.ts
@@ -47,15 +47,19 @@ type LeaderBoard = {
 const medals = ["🥇", "🥈", "🥉", "🪙", "🏵️", "🌹"];
 
 const getLanguage = (member: AoCMember, userMap: Record<string, UserMapEntry>): string => {
-    return userMap[member.id].language ?? "n/a";
+    const language = userMap[member.id].language ?? "n/a";
+    log.debug(`[AoC] Resolved ${language} for member with id ${member.id}`);
+    return language;
 };
 
 const getNameString = (member: AoCMember, userMap: Record<string, UserMapEntry>, includeLanguage: boolean): string => {
     const convertedName = userMap[member.id].displayName ?? member.name ?? `(anonymous user #${member.id})`;
     if(includeLanguage) {
         const language = getLanguage(member, userMap);
+        log.debug(`[AoC] Resolved ${convertedName} with language ${language} for member with id ${member.id}`);
         return `${convertedName} [${language}]`;
     }
+    log.debug(`[AoC] Resolved ${convertedName} for member with id ${member.id}`);
     return convertedName;
 };
 
