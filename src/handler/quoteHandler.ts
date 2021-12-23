cb9e9a84e4d5cb650dd1360b8aedaae76c9a6d11
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-12-23 17:46:51 +0100

    fix quote

--- src/handler/quoteHandler.ts
+++ src/handler/quoteHandler.ts
@@ -23,7 +23,7 @@ const isMessageAlreadyQuoted = (messageQuoter: readonly GuildMember[], client: C
     return messageQuoter.some(u => u.id === client.user!.id);
 };
 const hasMessageEnoughQuotes = (messageQuoter: readonly GuildMember[]): boolean => {
-    return messageQuoter.reduce((prev, curr) => isTrusted(curr) ? prev + 2 : prev + 1, 0) === quoteThreshold;
+    return messageQuoter.reduce((prev, curr) => isTrusted(curr) ? prev + 2 : prev + 1, 0) >= quoteThreshold;
 };
 const isQuoterQuotingHimself = (quoter: GuildMember, messageAuthor: GuildMember) => quoter.id === messageAuthor.id;
 const generateRandomColor = () => Math.floor(Math.random() * 16777215);
@@ -154,13 +154,23 @@ export const quoteReactionHandler = async(event: MessageReaction, user: User, cl
         return;
     }
 
-    if (reference !== undefined) {
-        const quoteMessage = await (<TextBasedChannels>targetChannel).send(reference);
-        await quoteMessage.reply(quote);
-    }
-    else {
-        await (<TextBasedChannels>targetChannel).send(quote);
-    }
+    // There is a small possiblity that quotes will be quoted multiple times
+    // This comes from the fact, that we're checking the preconditions at the start
+    // of this function, then perform rather time-consuming tasks. In the meantime
+    // another quote event could sneak in and performing a quote itself.
+    // Therefore we're checking again whether the message is already quoted BEFORE
+    // sending the quote.
+    // This is a really dirty fix - not even a fix at all - but I'm to lazy to
+    // introduce some proper synchronization. Should work good enough for us.
+    if(!isMessageAlreadyQuoted(quotingMembers, client)) {
+        if (reference !== undefined) {
+            const quoteMessage = await (<TextBasedChannels>targetChannel).send(reference);
+            await quoteMessage.reply(quote);
+        }
+        else {
+            await (<TextBasedChannels>targetChannel).send(quote);
+        }
 
-    await quotedMessage.react(event.emoji);
+        await quotedMessage.react(event.emoji);
+    }
 };
