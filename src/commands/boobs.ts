e0d18d0e83c57b3cbb8f8f5fa16da833d7b262fa
PatchRequest <PatchRequest@PatchRequest.com>
2021-12-27 03:26:23 +0100

    Sollte gehen war zu Faul mir eine Testumgebung zu bauen
    Sollte gehen war zu Faul mir eine Testumgebung zu bauen

---
+++ src/commands/boobs.ts
@@ -0,0 +1,60 @@
+// @ts-ignore
+import { Client, Message, User } from "discord.js";
+import { CommandResult, MessageCommand } from "./command";
+import * as log from "../utils/logger";
+
+const categorys = {
+    "Knöpfe":". .",
+    "Toffifee":"○ ○",
+    "Zwergtittchen":"◯ ◯",
+    "Wird langsam was":"(Y)",
+    "Kann man mit arbeiten":"(.Y.)",
+    "Hand voll glück":"(.)(.)",
+    "Booba":"( . )( . )",
+    "Schläuche":"UU",
+    "Mommys Milkers":"（。 ㅅ  。）",
+    "Milchtüten":"( • )( • )",
+    "Richtig dicke Titten":"(  .  )(  .  )",
+    "Brachiale Euter":"(   .  )(   .   )",
+    "Ach du dickes Erbgut":"(    .    )(    .    )",
+    "Tod durch Booba":"(     .     )(     .     )",
+    "Ich bin im Himmel":"(      .      )(      .      )",
+    "Gamb":`
+    ⣿⣿⣿⢃⣿⣿⢟⣿⣿⣿⣿⣿⣮⢫⣿⣿⣿⣿⣿⣟⢿⠃⠄⢻⣿⣿
+    ⣿⣿⡿⣸⠟⣵⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣷⣄⢰⡄⢿⣿
+    ⣿⣿⡇⠏⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠹⡎⣿
+    ⠛⠿⠄⢰⠋⡉⠹⣿⣿⣿⣿⣿⣿⠙⣿⣿⣿⣿⣿⣿⡟⢁⠙⡆⢡⣿
+    ⡆⠄⣤⠈⢣⣈⣠⣿⣿⣿⣿⣿⠏⣄⠻⣿⣿⣿⣿⣿⣆⣈⣴⠃⣿⣿
+    ⢿⠄⣿⡇⠄⠙⠿⣿⡿⠿⢋⣥⣾⣿⣷⣌⠻⢿⣿⣿⡿⠟⣡⣾⣿⣿
+    `
+
+}
+
+const sendBoob = async( message: Message ): Promise<Message<boolean>> => {
+    
+    const category = Object.keys(categorys)[Math.floor(Math.random() * Object.keys(categorys).length)];
+    const boob = categorys[category];
+
+    return message.reply(category +"\n"+ boob);
+
+};
+
+
+
+export class PenisCommand implements MessageCommand {
+    name = "boob";
+    description = "Zeigt dir die deine Boobs mit Größe an";
+
+    async handleMessage(message: Message, _client: Client): Promise<CommandResult> {
+        const { author } = message;
+        const mention = message.mentions.users.first();
+        const userToMeasure = mention !== undefined ? mention : author;
+
+        log.debug(`${author.id} wants to measure boob of user ${userToMeasure.id}`);
+
+        
+        await sendBoob(message);
+        return;
+        
+    }
+}
