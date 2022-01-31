f792108c92fe3e5291d422849b350bcef720d730
Fionera <fionera@fionera.de>
2022-02-01 00:48:28 +0100

    feat: Add Nix-Support for MacOS

--- shell.nix
+++ shell.nix
@@ -4,7 +4,13 @@ stdenv.mkDerivation {
   name = "csz-bot";
   buildInputs = [
     nodejs-16_x
-  ];
+    pixman
+    pkg-config
+    cairo
+    pango
+  ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+        CoreText
+      ]);
   shellHook = ''
     export PATH="$PWD/node_modules/.bin/:$PATH"
   '';
