a0479292dcead2b913b6c6af9dbcc35c7ec5dfbb
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-02-10 19:02:45 +0100

    small refactor - or maybe disimprovement

--- src/commands/boobs.ts
+++ src/commands/boobs.ts
@@ -4,45 +4,103 @@ import Boob from "../storage/model/Boob";
 import { CommandResult, MessageCommand } from "./command";
 import log from "../utils/logger";
 
-/* eslint-disable quote-props */
-const sizes: Record<string, string> = {
-    "Knöpfe": ". .",
-    "Toffifee": "○ ○",
-    "Zwergtittchen": "◯ ◯",
-    "Wird langsam was": "(Y)",
-    "Süßtittchen": "(.Y.)",
-    // "Igelschnäuzchen": "",
-    // "gemeiner Spitztitterich": "",
-    "Hand voll glück": "(.)(.)",
-    "Booba": "( . )( . )",
-    "Tellernippel": "⊚⊚",
-    "Schlöpse": "UU",
-    "Mega Schlöpse": `
-    | |  | |
-    | |  | |
-    |\\_|  |\\_|
-    `,
-    "Mommys Milkers": "（。 ㅅ  。）",
-    "Milchtüten": "( • )( • )",
-    "Richtig dicke Titten": "(  .  )(  .  )",
-    "Brachiale Ultramelonen": "(   .  )(   .   )",
-    "Ach du dickes Erbgut": "(    .    )(    .    )",
-    "Tod durch Booba": "(     .     )(     .     )",
-    "Ich bin im Himmel": "(      .      )(      .      )",
-    "Gamb": `
-    ⣿⣿⣿⢃⣿⣿⢟⣿⣿⣿⣿⣿⣮⢫⣿⣿⣿⣿⣿⣟⢿⠃⠄⢻⣿⣿
-    ⣿⣿⡿⣸⠟⣵⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣷⣄⢰⡄⢿⣿
-    ⣿⣿⡇⠏⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠹⡎⣿
-    ⠛⠿⠄⢰⠋⡉⠹⣿⣿⣿⣿⣿⣿⠙⣿⣿⣿⣿⣿⣿⡟⢁⠙⡆⢡⣿
-    ⡆⠄⣤⠈⢣⣈⣠⣿⣿⣿⣿⣿⠏⣄⠻⣿⣿⣿⣿⣿⣆⣈⣴⠃⣿⣿
-    ⢿⠄⣿⡇⠄⠙⠿⣿⡿⠿⢋⣥⣾⣿⣷⣌⠻⢿⣿⣿⡿⠟⣡⣾⣿⣿
-    `
+interface Booba {
+    description: string;
+    representation: string;
+}
+
+const boobas: Record<number, Booba> = {
+    0: {
+        description: "Knöpfe",
+        representation: ". ."
+    },
+    1: {
+        description: "Toffifee",
+        representation: "○ ○"
+    },
+    2: {
+        description: "Zwergtittchen",
+        representation: "◯ ◯"
+    },
+    3: {
+        description: "Wird langsam was",
+        representation: "(Y)"
+    },
+    4: {
+        description: "Süßtittchen",
+        representation: "(.Y.)"
+    },
+    5: {
+        description: "Hand voll glück",
+        representation: "(.)(.)"
+    },
+    6: {
+        description: "Booba",
+        representation: "( . )( . )"
+    },
+    7: {
+        description: "Tellernippel",
+        representation: "⊚⊚"
+    },
+    8: {
+        description: "Schlöpse",
+        representation: "UU"
+    },
+    9: {
+        description: "Mega Schlöpse",
+        representation: `
+        | |  | |
+        | |  | |
+        |\\_|  |\\_|
+        `
+    },
+    10: {
+        description: "Mommys Milkers",
+        representation: "（。 ㅅ  。）"
+    },
+    11: {
+        description: "Milchtüten",
+        representation: "( • )( • )"
+    },
+    12: {
+        description: "Richtig dicke Titten",
+        representation: "(  .  )(  .  )"
+    },
+    13: {
+        description: "Brachiale Ultramelonen",
+        representation: "(   .  )(   .   )"
+    },
+    14: {
+        description: "Ach du dickes Erbgut",
+        representation: "(    .    )(    .    )"
+    },
+    15: {
+        description: "Tod durch Booba",
+        representation: "(     .     )(     .     )"
+    },
+    16: {
+        description: "Ich bin im Himmel",
+        representation: "(      .      )(      .      )"
+    },
+    17: {
+        description: "Tod durch Booba",
+        representation: `
+        ⣿⣿⣿⢃⣿⣿⢟⣿⣿⣿⣿⣿⣮⢫⣿⣿⣿⣿⣿⣟⢿⠃⠄⢻⣿⣿
+        ⣿⣿⡿⣸⠟⣵⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣷⣄⢰⡄⢿⣿
+        ⣿⣿⡇⠏⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠹⡎⣿
+        ⠛⠿⠄⢰⠋⡉⠹⣿⣿⣿⣿⣿⣿⠙⣿⣿⣿⣿⣿⣿⡟⢁⠙⡆⢡⣿
+        ⡆⠄⣤⠈⢣⣈⣠⣿⣿⣿⣿⣿⠏⣄⠻⣿⣿⣿⣿⣿⣆⣈⣴⠃⣿⣿
+        ⢿⠄⣿⡇⠄⠙⠿⣿⡿⠿⢋⣥⣾⣿⣷⣌⠻⢿⣿⣿⡿⠟⣡⣾⣿⣿
+        `
+    }
 };
 /* eslint-enable quote-props */
 
 const sendBoob = async(user: User, message: Message, size: number, measurement: Date = new Date()): Promise<Message<boolean>> => {
-    const sizeName = Object.keys(sizes)[size];
-    const sizeContent = sizes[sizeName];
+    const booba = boobas[size];
+    if(!booba) {
+        throw new Error(`Booba with size ${size} not defined`);
+    }
     const measuredAt = new Intl.DateTimeFormat("de-DE", {
         hour: "2-digit",
         minute: "2-digit",
@@ -50,7 +108,7 @@ const sendBoob = async(user: User, message: Message, size: number, measurement: 
         hour12: false
     }).format(measurement);
 
-    return message.reply(`${sizeName} von <@${user.id}>:\n${sizeContent}\n(Gemessen um ${measuredAt})`);
+    return message.reply(`${booba.description} von <@${user.id}>:\n${booba.representation}\n(Gemessen um ${measuredAt})`);
 };
 
 
@@ -92,7 +150,7 @@ export class BoobCommand implements MessageCommand {
         if(recentMeasurement === null) {
             log.debug(`No recent boob measuring of ${userToMeasure.id} found. Creating Measurement`);
 
-            const size = Math.floor(Math.random() * Object.keys(sizes).length);
+            const size = Math.floor(Math.random() * Object.keys(boobas).length);
 
             if(await isNewBiggestBoobs(size)) {
                 log.debug(`${userToMeasure} has the new biggest boobs with size ${size}`);
