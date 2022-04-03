b616b5ee4f3efa9a565526947ef5d6947f35b405
holzmaster <holzmaster@outlook.com>
2022-04-03 13:54:19 +0200

    Migrate listroles

--- src/commands/modcommands/listroles.ts
+++ src/commands/modcommands/listroles.ts
@@ -1,13 +1,11 @@
-// ========================= //
-// = Copyright (c) NullDev = //
-// ========================= //
+import type { CommandFunction } from "../../types";
 
 /**
  * Lists all server roles
- *
- * @type {import("../../types").CommandFunction}
  */
-export const run = async(client, message, args) => {
+export const run: CommandFunction = async (client, message, args) => {
+    if (!message.guild) return "Keine Guild-Message, lel"; // TODO: Remove as soon as we have ProcessableMessage as base
+
     const roleNames = message.guild.roles.cache
         .filter(element => String(element.name).toLowerCase() !== "@everyone")
         .map(element => element.name);
