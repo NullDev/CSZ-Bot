d96b68bf654af3226c81c9f54b2500c481ec3db4
holzmaster <holzmaster@outlook.com>
2022-04-04 00:13:14 +0200

    Simplify config loading

--- src/utils/configHandler.ts
+++ src/utils/configHandler.ts
@@ -4,56 +4,38 @@
 
 import * as fs from "fs";
 import * as path from "path";
-import { Config } from "../types";
+import type { Config } from "../types";
 
 import log from "../utils/logger";
 
-const packagefile = require(path.resolve("package.json"));
+const packageFile = require(path.resolve("package.json"));
 const configPath = path.resolve("config.json");
 
-const isValidJson = (obj: any): Boolean => {
-    try {
-        JSON.parse(obj);
-    }
-    catch (e) {
-        return false;
-    }
-    return true;
-};
-
-export const getConfig = function(): Config {
+export const getConfig = () => {
     if (!fs.existsSync(configPath)) {
         log.error("Config does not exist! Make sure you copy config.template.json and paste it as 'config.json'. Then configure it.");
         process.exit(1);
     }
 
-    let jsondata;
+    let jsonString;
     try {
-        jsondata = fs.readFileSync(configPath, "utf8");
+        jsonString = fs.readFileSync(configPath, "utf8");
     }
     catch (e) {
-        log.error(`Cannot read config file: ${e}`);
+        log.error("Cannot read config file:", e);
         process.exit(1);
     }
 
-    if (isValidJson(jsondata)) return JSON.parse(jsondata);
-
-    log.error("Config is not valid JSON. Stopping...");
-    return process.exit(1);
+    try {
+        return JSON.parse(jsonString) as Config;
+    }
+    catch (e) {
+        log.error("Config is not valid JSON. Stopping...", e);
+        return process.exit(1);
+    }
 };
 
-export const getVersion = (): string => {
-    return packagefile.version;
-};
-
-export const getName = (): string => {
-    return packagefile.name;
-};
-
-export const getAuthor = (): string => {
-    return packagefile.author;
-};
-
-export const getDescription = (): string => {
-    return packagefile.description;
-};
+export const getVersion = () => packageFile.version;
+export const getName = () => packageFile.name;
+export const getAuthor = () => packageFile.author;
+export const getDescription = () => packageFile.description;
