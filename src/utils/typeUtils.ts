eb7f1ceab305a0fb121a430432abfd3572c05549
holzmaster <holzmaster@outlook.com>
2022-04-04 21:11:11 +0200

    Add `removeSuffix`

--- src/utils/typeUtils.ts
+++ src/utils/typeUtils.ts
@@ -6,3 +6,18 @@
 export function assertNever(v: never): never {
     throw new Error("This code should not be reached.");
 }
+
+/**
+ * Removes the TSuffix from entries in the config on type-level
+ *
+ * RemoveSuffix<"lol_id", "_id"> will map to "lol"
+ */
+export type RemoveSuffix<TString extends string, TSuffix extends string> = TString extends `${infer T}${TSuffix}`
+    ? T
+    : never;
+
+export function removeSuffix<T extends string, TSuffix extends string>(value: T, suffix: TSuffix): RemoveSuffix<T, TSuffix> {
+    console.assert(value.endsWith(suffix));
+    return value.slice(0, 0 - suffix.length) as RemoveSuffix<T, TSuffix>;
+}
+
