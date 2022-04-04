a14b9ac1874c607ca1ca2b1ccebfb7e0ad5366f2
holzmaster <holzmaster@outlook.com>
2022-04-04 21:46:03 +0200

    Add RemoveOptionalSuffix

--- src/utils/typeUtils.ts
+++ src/utils/typeUtils.ts
@@ -11,13 +11,32 @@ export function assertNever(v: never): never {
  * Removes the TSuffix from entries in the config on type-level
  *
  * RemoveSuffix<"lol_id", "_id"> will map to "lol"
+ * RemoveSuffix<"lol", "_id"> will map to never
  */
 export type RemoveSuffix<TString extends string, TSuffix extends string> = TString extends `${infer T}${TSuffix}`
     ? T
     : never;
 
 export function removeSuffix<T extends string, TSuffix extends string>(value: T, suffix: TSuffix): RemoveSuffix<T, TSuffix> {
-    console.assert(value.endsWith(suffix));
+    if (!value.endsWith(suffix)) {
+        throw new Error(`Expected "${value}" to end with "${suffix}"`);
+    }
     return value.slice(0, 0 - suffix.length) as RemoveSuffix<T, TSuffix>;
 }
 
+
+/**
+ * Removes the TSuffix from entries in the config on type-level if it is present
+ *
+ * RemoveOptionalSuffix<"lol_id", "_id"> will map to "lol"
+ * RemoveOptionalSuffix<"lol_id", "test"> will map to "lol_id"
+ */
+export type RemoveOptionalSuffix<TString extends string, TSuffix extends string> = TString extends `${infer T}${TSuffix}`
+    ? T
+    : TString;
+
+export function removeOptionalSuffix<T extends string, TSuffix extends string>(value: T, suffix: TSuffix): RemoveOptionalSuffix<T, TSuffix> {
+    return value.endsWith(suffix)
+        ? value.slice(0, 0 - suffix.length) as RemoveOptionalSuffix<T, TSuffix>
+        : value as RemoveOptionalSuffix<T, TSuffix>;
+}
