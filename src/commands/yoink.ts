6ee88d4b0698744e05ce1adcd5b8236d52037f60
Feluin <pascal.roecker.1997@gmail.com>
2022-06-14 17:48:17 +0200

    Eslint ist doof

--- src/commands/yoink.ts
+++ src/commands/yoink.ts
@@ -21,13 +21,12 @@ export const run: CommandFunction = async(client, message, args) => {
     if (args.length >= 1) {
         const parseEmoji = Util.parseEmoji(args[0]);
 
-        if(parseEmoji!==null){
-            let extension= parseEmoji.animated?".gif":".png";
-            let emoteUrl=`https://cdn.discordapp.com/emojis/${parseEmoji.id}`+extension;
-            let emotename= args.length>=2?args[1]:parseEmoji.name;
-            message.guild?.emojis.create(emoteUrl,emotename).then((emote) =>  message.channel.send(`Hab \<:${emote.name}:${emote.id}\> als \`${emote.name}\` hinzugefügt`));
+        if(parseEmoji !== null){
+            let extension = parseEmoji.animated ? ".gif" : ".png";
+            let emoteUrl = `https://cdn.discordapp.com/emojis/${parseEmoji.id}` + extension;
+            let emotename = args.length >= 2 ? args[1] : parseEmoji.name;
+            message.guild?.emojis.create(emoteUrl, emotename).then((emote) =>  message.channel.send(`Hab \<:${emote.name}:${emote.id}\> als \`${emote.name}\` hinzugefügt`));
         }
-
     }
     else {
         await message.channel.send("Argumente musst du schon angeben, du Mongo");
