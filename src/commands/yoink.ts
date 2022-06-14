d09263a00d2b96cb0ae3bfad989abb3f58a5f6b7
Feluin <pascal.roecker.1997@gmail.com>
2022-06-14 17:32:21 +0200

    Yoink alter

---
+++ src/commands/yoink.ts
@@ -0,0 +1,38 @@
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+import type { CommandFunction } from "../types";
+// Dependencies
+import {Util} from "discord.js";
+import {isTrusted} from "../utils/userUtils";
+
+
+/**
+ * Sends instructions on how to ask better questions
+ */
+export const run: CommandFunction = async(client, message, args) => {
+    // parse options
+    const pinger = message.guild?.members.cache.get(message.member!.user.id)!;
+    if (!isTrusted(pinger)){
+        await message.channel.send("Bist nicht cool genug");
+    }
+
+    if (args.length >= 1) {
+        const parseEmoji = Util.parseEmoji(args[0]);
+
+        if(parseEmoji!==null){
+            let extension= parseEmoji.animated?".gif":".png";
+            let emoteUrl=`https://cdn.discordapp.com/emojis/${parseEmoji.id}`+extension;
+            let emotename= args.length>=2?args[1]:parseEmoji.name;
+            message.guild?.emojis.create(emoteUrl,emotename).then((emote) =>  message.channel.send(`Hab \<:${emote.name}:${emote.id}\> als \`${emote.name}\` hinzugefügt`));
+        }
+
+    }
+    else {
+        await message.channel.send("Argumente musst du schon angeben, du Mongo");
+    }
+    await message.delete();
+};
+
+export const description = "Klaut emotes";
