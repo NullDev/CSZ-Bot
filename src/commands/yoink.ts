db31c8ba7fc30002a721b28cadbc14fab861afb5
Tobi <22715034+twobiers@users.noreply.github.com>
2022-06-15 15:15:13 +0200

    One time with professionals

--- src/commands/yoink.ts
+++ src/commands/yoink.ts
@@ -71,32 +71,8 @@ export class YoinkCommand implements MessageCommand, ApplicationCommand {
         const emoteUrl = `https://cdn.discordapp.com/emojis/${parseEmoji.id}` + extension;
         const emotename = name !== null ? name : parseEmoji.name;
 
-        guild?.emojis.create(emoteUrl, emotename).then((emote) => channel.send(`Hab \<:${emote.name}:${emote.id}\> als \`${emote.name}\` hinzugefügt`));
+        await guild?.emojis.create(emoteUrl, emotename).then((emote) => channel.send(`Hab \<:${emote.name}:${emote.id}\> als \`${emote.name}\` hinzugefügt`));
     }
 }
-
-export const run: CommandFunction = async(client, message, args) => {
-    // parse options
-    const pinger = message.guild?.members.cache.get(message.member!.user.id)!;
-    if (!isEmotifizierer(pinger)) {
-        await message.channel.send("Bist nicht cool genug");
-        return;
-    }
-
-    if (args.length >= 1) {
-        const parseEmoji = Util.parseEmoji(args[0]);
-
-        if (parseEmoji !== null) {
-            let extension = parseEmoji.animated ? ".gif" : ".png";
-            let emoteUrl = `https://cdn.discordapp.com/emojis/${parseEmoji.id}` + extension;
-            let emotename = args.length >= 2 ? args[1] : parseEmoji.name;
-            message.guild?.emojis.create(emoteUrl, emotename).then((emote) => message.channel.send(`Hab \<:${emote.name}:${emote.id}\> als \`${emote.name}\` hinzugefügt`));
-        }
-    }
-    else {
-        await message.channel.send("Argumente musst du schon angeben, du Mongo");
-    }
-    await message.delete();
-};
 
 export const description = "Klaut emotes";
