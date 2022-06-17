e8cf25311a2804620c1ded92ed556cb4c5ae8f84
twobiers <22715034+twobiers@users.noreply.github.com>
2022-06-17 17:21:11 +0200

    i18n for dadjoke

--- src/commands/special/dadJoke.ts
+++ src/commands/special/dadJoke.ts
@@ -3,46 +3,83 @@ import type { ProcessableMessage } from "../../handler/cmdHandler";
 import { substringAfter } from "../../utils/stringUtils";
 import { SpecialCommand, CommandResult } from "../command";
 
+type Lang = "german" | "austrian";
+
+interface PhraseConfig {
+    lang: Lang;
+}
+
+type Slot = "WHOIS" | "BOTNAME";
+
+
 export class DadJokeCommand implements SpecialCommand {
     cooldownTime: number | undefined;
     name: string = "Dad Joke";
     description: string = "Macht bei der passenden Gelegenheit einen Dad Joke";
-    randomness = 0.1;
+    randomness = 1.0;
+
+    matchPhrases: Record<string, PhraseConfig> = {
+        "ich bin": { lang: "german" },
+        "i bin": { lang: "austrian" },
+        "bin i": { lang: "austrian" }
+    };
+
+    answers: Record<Lang, string[]> = {
+        german: [
+            "Und ich bin der Uwe, ich bin auch dabei",
+            "Hallo #WHOIS#, ich bin #BOTNAME#"
+        ],
+        austrian: [
+            "Und i bin da #BOTNAME# oida",
+            "Oida #WHOIS# was geht?"
+        ]
+    };
+
+    private getRandomAnswer(config: PhraseConfig, slotConfig: Record<Slot, string>): string {
+        const langArr = this.answers[config.lang];
+        const randomIdx = Math.random() * langArr.length;
+        let randomMessage = langArr[randomIdx];
+
+        if(randomMessage.includes("#")) {
+            for(const slot of Object.keys(slotConfig)) {
+                if(randomMessage.includes(`#${slot}#`)) {
+                    randomMessage = randomMessage.replaceAll(`#${slot}#`, slotConfig[slot as Slot]);
+                }
+            }
+        }
+
+        return randomMessage;
+    }
 
 
     matches(message: ProcessableMessage): boolean {
         const msg = message.content.toLowerCase();
 
-        return msg.startsWith("ich bin") && substringAfter(msg, "ich bin").length > 3;
+        // Note the whitespaces
+        return Object.keys(this.matchPhrases).some(phrase => msg.startsWith(`${phrase} `) && substringAfter(msg, `${phrase} `).length > 3);
     }
 
     async handleSpecialMessage(message: ProcessableMessage, client: Client<boolean>): Promise<CommandResult> {
-        const idx = message.content.toLowerCase().lastIndexOf("ich bin ");
+        const phrase = Object.keys(this.matchPhrases).find(p => message.content.startsWith(p))!;
+        const attributes = this.matchPhrases[phrase];
+        const idx = message.content.toLowerCase().lastIndexOf(phrase);
+
         if(idx < (message.content.length - 1)) {
             // Get index of the first terminator character after trigger
             const indexOfTerminator = message.content.search(/(?:(?![,])[\p{P}\p{S}\p{C}])/gu);
             // Extract the dad joke subject
             const trimmedWords = message.content.substring(idx + 8, indexOfTerminator !== -1 ? indexOfTerminator : message.content.length).split(/\s+/).map(w => w.trim());
+            const whoIs = Util.cleanContent(trimmedWords.join(" "), message.channel).trim();
+            const slots: Record<Slot, string> = {
+                WHOIS: whoIs,
+                BOTNAME: client.user!.username
+            };
 
-            const randomUwe = Math.random() < 0.01;
+            const answer = this.getRandomAnswer(attributes, slots);
 
-            if(randomUwe) {
-                await message.reply({
-                    content: "Und ich bin der Uwe, ich bin auch dabei"
-                });
-                return;
-            }
-
-            if(trimmedWords.length > 0 && trimmedWords.length <= 10) {
-                const whoIs = Util.cleanContent(trimmedWords.join(" "), message.channel).trim();
-
-                if(whoIs.length > 0) {
-                    await message.reply({
-                        content: `Hallo ${whoIs}, ich bin ${client.user!.username}.`
-                    });
-                    return;
-                }
-            }
+            await message.reply({
+                content: answer
+            });
         }
     }
 }
