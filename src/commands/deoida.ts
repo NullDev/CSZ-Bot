e76ca44b579f12711abadecbea70aff5a187f7cb
holzmaster <holzmaster@outlook.com>
2022-07-05 03:17:03 +0200

    Fix param parsing

--- src/commands/deoida.ts
+++ src/commands/deoida.ts
@@ -23,17 +23,22 @@ const simpleRules: readonly Rule[] = [
 
 
 function deOida(value: string): Promise<string> {
+    // We cannot just split all words using \s*. That could tear apart words or translations like "fescher bub"
+    // Also, we need to take line breaks into account. We assume that tokens that are one translation unit
+    // won't get torn apart by a line break.
+    // This also reduces the number of combinations to check
+
+    // TODO: Look up something in database
+
     let result = value;
     for (const { pattern, translation } of simpleRules) {
         result = result.replaceAll(pattern, translation as string);
     }
 
-    // TODO: Look up something in database
-
     return Promise.resolve(result);
 }
 
-export const run: CommandFunction = async(_client, message, _args, _context) => {
+export const run: CommandFunction = async(_client, message, args, _context) => {
     const messageToTranslate = message.reference?.messageId
         ? (await message.channel.messages.fetch(message.reference.messageId))
         : message;
@@ -42,7 +47,15 @@ export const run: CommandFunction = async(_client, message, _args, _context) => 
         return "Nichts zum Übersetzen da :question:";
     }
 
-    const translation = await deOida(messageToTranslate.content);
+    const textToTranslate = messageToTranslate === message
+        ? args.join(" ")
+        : messageToTranslate.content;
+
+    if (!textToTranslate) {
+        return "Nichts zum Übersetzen da :question:";
+    }
+
+    const translation = await deOida(textToTranslate);
 
     await messageToTranslate.reply(`🇦🇹 -> 🇩🇪: ${translation}`);
 };
