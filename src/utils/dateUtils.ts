211d5bb2488e2bbddf3bca4a56d7e863ff61e1fa
twobiers <22715034+twobiers@users.noreply.github.com>
2022-10-03 20:22:48 +0200

    iso time lol

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,20 +1,7 @@
-
-const timeFormatter = new Intl.DateTimeFormat("de-DE", {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: false
-});
-
-const dateTimeFormatter = new Intl.DateTimeFormat("de-DE", {
-    dateStyle: "long",
-    timeStyle: "medium"
-});
-
 export function formatTime(time: Date) {
-    return timeFormatter.format(time);
+    return time.toISOString();
 }
 
 export function formatDateTime(dateTime: Date) {
-    return dateTimeFormatter.format(dateTime);
+    return dateTime.toISOString();
 }
