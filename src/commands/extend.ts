75779647ac6480fb3a7bd5ac37090d81f4ce6b96
5yn74x <30578607+5yn74x@users.noreply.github.com>
2022-10-08 12:26:26 +0200

    fixing extend command to respect new field layout

--- src/commands/extend.ts
+++ src/commands/extend.ts
@@ -1,4 +1,4 @@
-import { APIEmbed, ChannelType, Message } from "discord.js";
+import {APIEmbed, APIEmbedField, ChannelType, Message} from "discord.js";
 
 import log from "../utils/logger.js";
 import { getConfig } from "../utils/configHandler.js";
@@ -15,6 +15,7 @@ export const run: CommandFunction = async(client, message, args, context) => {
     if (message.reference.guildId !== context.guild.id || !message.reference.channelId) return "Bruder bleib mal hier auf'm Server.";
 
     if (!message.reference.messageId) return "Die Nachricht hat irgendwie keine reference-ID";
+    if (!args.length) return "Bruder da sind keine Antwortmöglichkeiten :c";
 
     const channel = context.guild.channels.cache.get(message.reference.channelId);
 
@@ -35,55 +36,44 @@ export const run: CommandFunction = async(client, message, args, context) => {
     if (!replyMessage.editable) return "Bruder aus irgrndeinem Grund hat der Bot verkackt und kann die Umfrage nicht bearbeiten :<";
     if (replyMessage.embeds[0].color !== 3066993) return "Bruder die Umfrage ist nicht erweiterbar (ง'̀-'́)ง";
 
-    const oldPollOptions = replyMessage.embeds[0].description!.split("\n");
-
-    if (oldPollOptions.length === poll.OPTION_LIMIT) return "Bruder die Umfrage ist leider schon voll (⚆ ͜ʖ⚆)";
-
-    const oldPollOptionsLength = replyMessage.embeds[0].description!.length;
-    if (oldPollOptionsLength > poll.TEXT_LIMIT) return "Bruder die Umfrage ist leider schon voll (⚆ ͜ʖ⚆)";
-
-    for (let i = 0; i < oldPollOptions.length; ++i) {
-        if (!oldPollOptions[i].startsWith(poll.LETTERS[i])) {
-            return "Bruder das ist keine Umfrage ಠ╭╮ಠ";
-        }
-    }
-
-    if (!args.length) return "Bruder da sind keine Antwortmöglichkeiten :c";
+    const oldPollOptionFields = replyMessage.embeds[0].fields.filter(field => !field.inline || field.name.startsWith("Antwortoption"));
+    if (oldPollOptionFields.length === poll.OPTION_LIMIT) return "Bruder die Umfrage ist leider schon voll (⚆ ͜ʖ⚆)";
 
     const additionalPollOptions = args.join(" ")
         .split(";")
         .map(e => e.trim())
         .filter(e => e.replace(/\s/g, "") !== "");
 
-    let additionalPollOptionsLength = 0;
-    for (const additionalPollOption of additionalPollOptions) {
-        additionalPollOptionsLength += additionalPollOption.length;
-    }
-
     if (!additionalPollOptions.length) return "Bruder da sind keine Antwortmöglichkeiten :c";
-    if (oldPollOptionsLength + additionalPollOptionsLength > poll.TEXT_LIMIT) return "Bruder die Umfrage ist zu lang";
-    if (oldPollOptions.length + additionalPollOptions.length > poll.OPTION_LIMIT) return `Bruder mit deinen Antwortmöglichkeiten wird das Limit von ${poll.OPTION_LIMIT} überschritten!`;
+    else if (additionalPollOptions.length + oldPollOptionFields.length > poll.OPTION_LIMIT) return `Bruder mit deinen Antwortmöglichkeiten wird das Limit von ${poll.OPTION_LIMIT} überschritten!`;
+    else if (additionalPollOptions.some(value => value.length > poll.FIELD_VALUE_LIMIT)) return `Bruder mindestens eine Antwortmöglichkeit ist länger als ${poll.FIELD_VALUE_LIMIT} Zeichen!`;
 
     const originalAuthor = replyMessage.embeds[0].author!.name.split(" ")[2];
     const authorNote = originalAuthor !== message.author.username ? ` (von ${message.author.username})` : "";
 
+    const newFields: APIEmbedField[] = additionalPollOptions.map((value, i) => {
+        return {name: `Antwortoption ${poll.LETTERS[oldPollOptionFields.length + i]} ${authorNote}`, value, inline: false};
+    });
+
+    let metaFields = replyMessage.embeds[0].fields.filter(field => field.inline || !field.name.startsWith("Antwortoption"));
     // TODO: Replace with structuredClone as soon as we run on Node.js 18
     const embed = JSON.parse(JSON.stringify(replyMessage.embeds[0])) as APIEmbed;
-    embed.description += "\n";
-    additionalPollOptions.forEach((e, i) => (embed.description += `${poll.LETTERS[oldPollOptions.length + i]} - ${e}${authorNote}\n`));
 
-    if (oldPollOptions.length + additionalPollOptions.length === poll.OPTION_LIMIT) {
+    if (oldPollOptionFields.length + additionalPollOptions.length === poll.OPTION_LIMIT) {
         embed.color = 0xCD5C5C;
-        embed.footer = undefined;
+        metaFields = metaFields.filter(field => !field.name.endsWith("Erweiterbar"));
     }
+
+    embed.fields = [...oldPollOptionFields, ...newFields, ...metaFields];
 
     const msg = await replyMessage.edit({
         embeds: [embed]
     });
+
     for (const i in additionalPollOptions) {
         // Disabling rule because the order is important
         // eslint-disable-next-line no-await-in-loop
-        await msg.react(poll.EMOJI[oldPollOptions.length + Number(i)]);
+        await msg.react(poll.EMOJI[oldPollOptionFields.length + Number(i)]);
     }
     await message.delete();
 };
