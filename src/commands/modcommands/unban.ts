c469ee2f3486e2b5c0ccbbb47cea0ca404738867
Tobi <22715034+twobiers@users.noreply.github.com>
2022-11-27 13:52:55 +0100

    Update src/commands/modcommands/unban.ts

--- src/commands/modcommands/unban.ts
+++ src/commands/modcommands/unban.ts
@@ -26,7 +26,7 @@ export class UnbanCommand implements ApplicationCommand, MessageCommand {
         return new SlashCommandBuilder()
             .setName(this.name)
             .setDescription(this.description)
-            .setDefaultMemberPermissions(undefined)
+            .setDefaultMemberPermissions(0)
             .addUserOption(new SlashCommandUserOption()
                 .setRequired(true)
                 .setName("user")
