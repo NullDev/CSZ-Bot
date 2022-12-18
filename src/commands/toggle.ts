5ac1a3ef6aff093a883f88f386065cd3f885133e
twobiers <22715034+twobiers@users.noreply.github.com>
2022-12-18 13:39:32 +0100

    toggle for sdm

--- src/commands/toggle.ts
+++ src/commands/toggle.ts
@@ -1,4 +1,4 @@
-import { APIEmbedField, EmbedBuilder } from "discord.js";
+import { APIEmbedField, EmbedBuilder, Message } from "discord.js";
 
 import * as poll from "./poll.js";
 import type { CommandFunction } from "../types.js";
@@ -6,6 +6,47 @@ import { isMod } from "../utils/userUtils.js";
 
 const isPollField = (field: APIEmbedField): boolean =>
     !field.inline && poll.LETTERS.some((l) => field.name.startsWith(l));
+
+const togglePoll = async(message: Message) => {
+    const pollEmbed = message.embeds[0];
+    const pollOptions = pollEmbed.fields.filter((field) => isPollField(field));
+    const isFull = pollOptions.length === poll.OPTION_LIMIT;
+    if (isFull) return "Bruder die ist eh schon voll :<";
+
+    const extendColor = 3066993;
+    const isExtensible = pollEmbed.color !== extendColor;
+
+    const colorToUse = isExtensible ? extendColor : null;
+
+    const embed = EmbedBuilder.from(message.embeds[0]);
+    embed.setColor(colorToUse);
+
+    await Promise.all([
+        message.edit({
+            embeds: [embed]
+        }),
+        message.delete()
+    ]);
+};
+
+const toggleSdm = async(message: Message) => {
+    const embed = message.embeds[0];
+    const isYes = embed.color === 0x2ecc71;
+
+    const toggledEmbed = EmbedBuilder.from(message.embeds[0]);
+    toggledEmbed.setColor(isYes ? 0xe74c3c : 0x2ecc71);
+    const file = isYes ? "no.png" : "yes.png";
+    toggledEmbed.setThumbnail(`"attachment://${file}`);
+
+    await Promise.all([
+        message.edit({
+            embeds: [toggledEmbed],
+            files: [`./assets/${file}`]
+        }),
+        message.delete()
+    ]);
+};
+
 
 /**
  * Extends an existing poll or strawpoll
@@ -35,26 +76,17 @@ export const run: CommandFunction = async(client, message, args, context) => {
         pollEmbed.author!.name.startsWith("Umfrage") ||
         pollEmbed.author!.name.startsWith("Strawpoll");
 
-    if (!isPoll) return "Bruder das ist keine Umfrage :<";
+    const isSdm =
+        pollEmbed.author!.name.startsWith("Secure Decision");
 
-    const pollOptions = pollEmbed.fields.filter((field) => isPollField(field));
-    const isFull = pollOptions.length === poll.OPTION_LIMIT;
-    if (isFull) return "Bruder die ist eh schon voll :<";
+    if (isPoll) {
+        return togglePoll(replyMessage);
+    }
+    if (isSdm) {
+        return toggleSdm(replyMessage);
+    }
 
-    const extendColor = 3066993;
-    const isExtensible = pollEmbed.color !== extendColor;
-
-    const colorToUse = isExtensible ? extendColor : null;
-
-    const embed = EmbedBuilder.from(replyMessage.embeds[0]);
-    embed.setColor(colorToUse);
-
-    await Promise.all([
-        replyMessage.edit({
-            embeds: [embed]
-        }),
-        message.delete()
-    ]);
+    return "Bruder da ist nichts was ich manipulieren kann";
 };
 
 export const description =
