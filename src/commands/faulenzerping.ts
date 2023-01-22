1be078021b8b66bd52cb47a7f75b642c6d6ba91f
holzmaster <holzmaster@outlook.com>
2023-01-22 23:52:28 +0100

    Remove unrelated stuff

--- src/commands/faulenzerping.ts
+++ src/commands/faulenzerping.ts
@@ -1,73 +1,20 @@
-import { CacheType, Client, CommandInteraction, GuildMember, Message, EmbedBuilder, SlashCommandBuilder, SlashCommandStringOption } from "discord.js";
+import type { Client, Message } from "discord.js";
 
 import { BotContext } from "../context.js";
-import { ApplicationCommand, MessageCommand } from "./command.js";
+import { MessageCommand } from "./command.js";
 import type { ProcessableMessage } from "../handler/cmdHandler.js";
 
-/**
- * Randomly capitalize letters
- */
-const transform = function(c: string): string {
-    if (c === "ß" || c === "ẞ") return c;
-    return Math.random() < 0.5 ? c.toLowerCase() : c.toUpperCase();
-};
-
-/**
- * Mocks text
- */
-const mock = (str: string): string => str.split("").map(transform).join("");
-
-/**
- * build mocked embed
- */
-const buildMock = (author: GuildMember, toMock: string) => {
-    return new EmbedBuilder()
-        .setDescription(`${mock(toMock)} <:mock:677504337769005096>`)
-        .setColor(0xFFC000)
-        .setAuthor({
-            name: `${author.displayName}`,
-            iconURL: author.displayAvatarURL()
-        });
-};
-
-export class MockCommand implements MessageCommand, ApplicationCommand {
+export class FaulenzerPingCommand implements MessageCommand {
     name = "faulenzerping";
-    description = "Pingt alle Hurensöhne, die noch nicht auf die ausgewählte Nachricht reagiert haben und in der angegebenen Gruppe sind.";
-    applicationCommand = new SlashCommandBuilder()
-        .setName(this.name)
-        .setDescription(this.description)
-        .addStringOption(
-            new SlashCommandStringOption()
-                .setName("text")
-                .setDescription("Wat soll ich denn mocken")
-                .setRequired(true)
-        );
-
-    async handleInteraction(command: CommandInteraction<CacheType>, _client: Client<boolean>, _context: BotContext): Promise<void> {
-        if (!command.isChatInputCommand()) {
-            // TODO: Solve this on a type level
-            return;
-        }
-
-        const author = command.guild?.members.resolve(command.user);
-        const text = command.options.getString("text")!;
-        if (!author) {
-            throw new Error("Couldn't resolve guild member");
-        }
-
-        const mockedEmbed = buildMock(author, text);
-        await command.reply({
-            embeds: [mockedEmbed]
-        });
-    }
+    description = "Pingt alle Leute, die noch nicht auf die ausgewählte Nachricht reagiert haben, aber in der angegebenen Gruppe sind.";
 
     // https://stackoverflow.com/a/64242640
     async getReactedUsers(message: ProcessableMessage) {
         // fetch the users
         // I STOPPED HERE BECAUSE IT SUXXXXX
-        message.reactions.cache.users.fetch().then((users) =>
+        message.reactions.cache.users.fetch().then(users =>
             // I'm not quite sure what you were trying to accomplish with the original lines
-            reaction.cache.map((item) => item.users.cache.array())
+            reaction.cache.map(item => item.users.cache.array())
         );
     }
 
@@ -76,6 +23,7 @@ export class MockCommand implements MessageCommand, ApplicationCommand {
         const { channel } = message;
 
         const isReply = message.reference?.messageId !== undefined;
+
         let content = message.content.slice(`${context.rawConfig.bot_settings.prefix.command_prefix}${this.name} `.length);
         const hasContent = !!content && content.trim().length > 0;
 
@@ -95,25 +43,6 @@ export class MockCommand implements MessageCommand, ApplicationCommand {
                 // eslint-disable-next-line prefer-destructuring
                 content = replyMessage.content;
             }
-        }
-
-        const mockedEmbed = buildMock(author, content);
-
-        if (isReply) {
-            await Promise.all([
-                replyMessage!.reply({
-                    embeds: [mockedEmbed]
-                }),
-                message.delete()
-            ]);
-        }
-        else {
-            await Promise.all([
-                channel.send({
-                    embeds: [mockedEmbed]
-                }),
-                message.delete()
-            ]);
         }
     }
 }
