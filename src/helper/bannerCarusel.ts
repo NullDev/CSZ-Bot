2f5b7573f93aab3f8230bd0ec13d963e65d1068c
twobiers <22715034+twobiers@users.noreply.github.com>
2023-02-21 20:46:48 +0100

    rotate banners every two months

---
+++ src/helper/bannerCarusel.ts
@@ -0,0 +1,32 @@
+import { BotContext } from "../context.js";
+import path from "node:path";
+import { readdir  } from "node:fs/promises";
+
+const bannersDir = path.resolve("banners");
+
+const pickRandomBanner = (files: string[]): string => {
+    const newBanner = files[Math.floor(Math.random() * files.length)];
+    return path.resolve(bannersDir, newBanner);
+};
+
+export const rotate = async(context: BotContext) => {
+    const currentHash = context.guild.banner;
+    const files = await readdir(bannersDir);
+
+    if(files.length === 0) {
+        return;
+    }
+
+    const file = pickRandomBanner(files);
+
+    await context.guild.setBanner(file);
+    const newHash = context.guild.banner;
+
+    // I'm too lazy to implement something like a persistence logic here.
+    // If the set banner is not updated (= is the same as before), just pick
+    // a new one.
+    if(newHash === currentHash && files.length > 1) {
+        const secondTry = pickRandomBanner(files.filter(f => f !== file));
+        await context.guild.setBanner(secondTry);
+    }
+};
