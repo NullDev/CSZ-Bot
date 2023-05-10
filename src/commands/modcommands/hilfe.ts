d7cc90a9b45e0fa486ac27ffe0756bf938460359
twobiers <22715034+twobiers@users.noreply.github.com>
2023-05-10 15:48:22 +0200

    rome fixes for hilfe.ts

--- src/commands/modcommands/hilfe.ts
+++ src/commands/modcommands/hilfe.ts
@@ -45,10 +45,7 @@ export const run: CommandFunction = async(_client, message, _args, context) => {
 
     // Add :envelope: reaction to authors message
     await message.author.send(
-        "Hallo, " + message.author + "!\n\n" +
-        "Hier ist eine Liste mit commands:\n\n```CSS\n" +
-        commandText +
-        "```"
+        `Hallo, ${message.author}!\n\nHier ist eine Liste mit commands:\n\n\`\`\`CSS\n${commandText}\`\`\``,
     );
     await message.react("✉"); // Send this last, so we only display a confirmation when everything actually worked
 };
