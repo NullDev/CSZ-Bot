ffb6ec75080779c5839636266b9477ce16a07d89
holzmaster <holzmaster@outlook.com>
2023-06-16 20:47:56 +0200

    Format

--- src/commands/stempelgraph.ts
+++ src/commands/stempelgraph.ts
@@ -287,7 +287,7 @@ export class StempelgraphCommand implements ApplicationCommand {
             );
 
             await command.reply({
-                content: `\`\`\`\n${dotSrc}\`\`\``,
+                content: "```\n" + dotSrc + "\n```",
                 files: [
                     {
                         attachment: stempelGraph,
