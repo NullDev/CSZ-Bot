cddb58a0ea90c914d3eb71154838ea234abaa213
holzmaster <holzmaster@outlook.com>
2023-09-08 00:10:17 +0200

    rome-ignore -> biome-ignore

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -48,7 +48,7 @@ const getContributors = async (): Promise<string> => {
 
 const getTechStackInfo = async (): Promise<string> => {
     return (
-        // rome-ignore lint/style/useTemplate: Better readability
+        // biome-ignore lint/style/useTemplate: Better readability
         `**Programmiersprache\n** ${(await fetchLanguages()).join(",")} \n` +
         `**NodeJS Version\n** ${process.version} \n`
     );
@@ -56,7 +56,7 @@ const getTechStackInfo = async (): Promise<string> => {
 
 const getSystemInfo = (): string => {
     return (
-        // rome-ignore lint/style/useTemplate: Better readability
+        // biome-ignore lint/style/useTemplate: Better readability
         `**PID\n** ${process.pid} \n` +
         `**Uptime\n** ${Math.floor(process.uptime())}s \n` +
         `**Platform\n** ${process.platform} \n` +
@@ -86,7 +86,7 @@ const getServerInfo = (guild: Guild): string => {
     const level = getServerLevel(guild);
 
     return (
-        // rome-ignore lint/style/useTemplate: Better readability
+        // biome-ignore lint/style/useTemplate: Better readability
         `**Mitglieder\n** ${guild.memberCount} / ${guild.maximumMembers} \n` +
         `**Oberbabo\n** <@!${guild.ownerId}> \n` +
         `**Geburtstag\n** ${birthday} \n` +
