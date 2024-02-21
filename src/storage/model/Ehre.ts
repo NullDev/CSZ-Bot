03a9a4c4cd260e420624d8ad696ee9ff44138610
twobiers <22715034+twobiers@users.noreply.github.com>
2024-02-21 21:22:26 +0100

    filter 0.0 out

--- src/storage/model/Ehre.ts
+++ src/storage/model/Ehre.ts
@@ -82,7 +82,7 @@ export class EhrePoints extends Model {
 
     static async getUserInGroups(): Promise<EhreGroups> {
         const { rows, count } = await EhrePoints.findAndCountAll({
-            where: { points: { [Op.gt]: 0 } },
+            where: { points: { [Op.gte]: 0.1 } },
             order: [["points", "DESC"]],
         });
 
