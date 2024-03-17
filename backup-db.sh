51fb04952598b6bfab9c76e89f17d8cee7eb94c2
holzmaster <holzmaster@outlook.com>
2024-03-18 00:20:57 +0100

    Compress backups

--- backup-db.sh
+++ backup-db.sh
@@ -3,4 +3,7 @@
 TODAY=$(date +"%Y-%m-%d")
 
 mkdir -p backups
-sqlite3 storage.db ".backup backups/$TODAY-backup.db"
+TARGET_FILE=backups/$TODAY-backup.db
+
+sqlite3 storage.db ".backup $TARGET_FILE"
+xz "$TARGET_FILE"
