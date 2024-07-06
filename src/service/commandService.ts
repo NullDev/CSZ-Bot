764b74ef661326da8d2010b1f5e231c1f8e73567
holzmaster <holzmaster@outlook.com>
2024-07-07 01:54:42 +0200

    Add support for legacy command loading

--- src/service/commandService.ts
+++ src/service/commandService.ts
@@ -3,6 +3,7 @@ import * as path from "node:path";
 
 import type { BotContext } from "../context.js";
 import type { Command } from "../commands/command.js";
+import type { LegacyCommand } from "../types.js";
 
 import log from "@log";
 
@@ -11,9 +12,41 @@ const commandExtensions = [".ts", ".js"];
 export async function readAvailableCommands(
     context: BotContext,
 ): Promise<Command[]> {
-    const commandFiles = await fs.readdir(context.commandDir);
+    const modules = loadRawCommandModules(context, context.commandDir);
 
     const res = [];
+    for await (const module of modules) {
+        if (!module.default) {
+            continue;
+        }
+        res.push(new module.default());
+    }
+    return res;
+}
+
+export async function readAvailableLegacyCommands(
+    context: BotContext,
+    type: "mod" | "pleb",
+): Promise<LegacyCommand[]> {
+    const dir = type === "mod" ? context.modCommandDir : context.commandDir;
+    const modules = loadRawCommandModules(context, dir);
+
+    const res = [];
+    for await (const module of modules) {
+        if (
+            !module.description ||
+            module.description !== "string" ||
+            typeof module.run !== "function"
+        ) {
+            continue;
+        }
+        res.push(module);
+    }
+    return res;
+}
+
+async function* loadRawCommandModules(context: BotContext, commandDir: string) {
+    const commandFiles = await fs.readdir(commandDir);
 
     for (const file of commandFiles) {
         if (!commandExtensions.some(extension => file.endsWith(extension))) {
@@ -24,19 +57,6 @@ export async function readAvailableCommands(
         moduleUrl.pathname = path.join(context.commandDir, file);
         log.debug(`Trying to load ${moduleUrl}`);
 
-        const module = await import(moduleUrl.toString());
-        if (!module.default) {
-            continue;
-        }
-
-        const instance = new module.default();
-        if (!instance.name) {
-            log.warn(instance, `Command ${file} has no name, skipping`);
-            continue;
-        }
-
-        res.push(instance);
+        yield await import(moduleUrl.toString());
     }
-
-    return res;
 }
