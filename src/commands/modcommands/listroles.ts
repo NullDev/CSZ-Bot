3a8de1068c48fe9178927f9214279ffecf7b06a9
holzmaster <holzmaster@outlook.com>
2024-07-07 15:37:05 +0200

    Migrate listroles

--- src/commands/modcommands/listroles.ts
+++ src/commands/modcommands/listroles.ts
@@ -1,14 +1,16 @@
-import type { CommandFunction } from "../../types.js";
+import type { MessageCommand } from "../command.js";
+import type { BotContext } from "../../context.js";
+import type { ProcessableMessage } from "../../service/commandService.js";
 
-/**
- * Lists all server roles
- */
-export const run: CommandFunction = async (message, _args) => {
-    const roleNames = message.guild.roles.cache
-        .filter(element => String(element.name).toLowerCase() !== "@everyone")
-        .map(element => element.name);
+export default class ToggleCommand implements MessageCommand {
+    modCommand = true;
+    name = "listroles";
+    description = "Listet alle server rollen auf";
+    async handleMessage(message: ProcessableMessage, context: BotContext): Promise<void> {
+        const roleNames = message.guild.roles.cache
+            .filter(element => String(element.name).toLowerCase() !== "@everyone")
+            .map(element => element.name);
 
-    await message.channel.send(`Roles: \n\n${roleNames.join(", ")}`);
-};
-
-export const description = "Listet alle server rollen auf";
+        await message.channel.send(`Roles: \n\n${roleNames.join(", ")}`);
+    }
+}
