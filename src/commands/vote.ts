dd908e64993b6bc1a846608a2ad741a74a8fc6cf
holzmaster <holzmaster@outlook.com>
2024-07-07 14:52:26 +0200

    Migrate vote

--- src/commands/vote.ts
+++ src/commands/vote.ts
@@ -1,7 +1,12 @@
 import { parseArgs, type ParseArgsConfig } from "node:util";
+
 import { cleanContent } from "discord.js";
 
-import type { CommandFunction } from "../types.js";
+import type { MessageCommand } from "./command.js";
+import type { BotContext } from "../context.js";
+import type { ProcessableMessage } from "../service/commandService.js";
+
+import { parseLegacyMessageParts } from "../service/commandService.js";
 
 const argsConfig = {
     options: {
@@ -15,42 +20,46 @@ const argsConfig = {
     allowPositionals: true,
 } satisfies ParseArgsConfig;
 
-/**
- * Creates a new poll (vote; yes/no)
- */
-export const run: CommandFunction = async (message, args, context) => {
-    const { values: options, positionals } = parseArgs({ ...argsConfig, args });
-
-    if (positionals.length === 0) {
-        return "Bruder da ist keine Frage :c";
-    }
-
-    const question = positionals.join(" ");
-    if (question.length > 4096) {
-        return "Bruder die Frage ist ja länger als mein Schwanz :c";
-    }
-
-    const embed = {
-        description: `**${cleanContent(question, message.channel)}**`,
-        timestamp: new Date().toISOString(),
-        color: 0x9400d3,
-        author: {
-            name: `Umfrage von ${message.author.username}`,
-            icon_url: message.author.displayAvatarURL(),
-        },
-    };
-
-    const channel = options.channel ? context.textChannels.votes : message.channel;
-
-    const messageWithVoteContent = await channel.send({
-        embeds: [embed],
-    });
-    await Promise.all([messageWithVoteContent.react("👍"), messageWithVoteContent.react("👎")]);
-    await message.delete();
-};
-
-export const description = `Erstellt eine Umfrage (ja/nein).
+export default class VoteCommand implements MessageCommand {
+    modCommand = false;
+    name = "min";
+    description = `Erstellt eine Umfrage (ja/nein).
 Usage: $COMMAND_PREFIX$vote [Optionen?] [Hier die Frage]
 Optionen:
 \t-c, --channel
 \t\t\tSendet die Umfrage in den Umfragenchannel, um den Slowmode zu umgehen`;
+
+    async handleMessage(message: ProcessableMessage, context: BotContext): Promise<void> {
+        const { args } = parseLegacyMessageParts(context, message);
+        const { values: options, positionals } = parseArgs({ ...argsConfig, args });
+
+        if (positionals.length === 0) {
+            await message.channel.send("Bruder da ist keine Frage :c");
+            return;
+        }
+
+        const question = positionals.join(" ");
+        if (question.length > 4096) {
+            await message.channel.send("Bruder die Frage ist ja länger als mein Schwanz :c");
+            return;
+        }
+
+        const embed = {
+            description: `**${cleanContent(question, message.channel)}**`,
+            timestamp: new Date().toISOString(),
+            color: 0x9400d3,
+            author: {
+                name: `Umfrage von ${message.author.username}`,
+                icon_url: message.author.displayAvatarURL(),
+            },
+        };
+
+        const channel = options.channel ? context.textChannels.votes : message.channel;
+
+        const messageWithVoteContent = await channel.send({
+            embeds: [embed],
+        });
+        await Promise.all([messageWithVoteContent.react("👍"), messageWithVoteContent.react("👎")]);
+        await message.delete();
+    }
+}
