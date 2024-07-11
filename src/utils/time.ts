c194c35ab5adb01841519df1f36ef7b4f013e8fe
holzmaster <holzmaster@outlook.com>
2024-07-11 14:42:57 +0200

    Document time utils

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,15 +1,19 @@
+/** @returns The amount of time in milliseconds */
 export function seconds(n: number): number {
     return n * 1000;
 }
 
+/** @returns The amount of time in milliseconds */
 export function minutes(n: number): number {
     return seconds(n * 60);
 }
 
+/** @returns The amount of time in milliseconds */
 export function hours(n: number): number {
     return minutes(n * 60);
 }
 
+/** @returns The amount of time in milliseconds */
 export function days(n: number): number {
     return hours(n * 24);
 }
