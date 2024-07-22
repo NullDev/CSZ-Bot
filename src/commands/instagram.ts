db95c384eb1263bac35e9a49409d4fc2b1830a0e
SoraTenshi <dream@neoncity.dev>
2024-07-22 14:06:48 +0200

    fix instagram regex

--- src/commands/instagram.ts
+++ src/commands/instagram.ts
@@ -5,7 +5,7 @@ import type { BotContext } from "../context.js";
 import * as instagramService from "../service/instagramService.js";
 
 const instagramOptions = {
-    uriPattern: /(?<uri>https?:\/\/(www\.)?instagram\.com\/(?:reel|tv|p)\/.*)\/.*/i,
+    uriPattern: /(?<uri>https?:\/\/(www\.)?instagram\.com\/(?:reel|tv|p)\/.*)\/?.*/i,
     headers: {
         "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.124 Safari/537.36 Edg/102.0.1245.44",
