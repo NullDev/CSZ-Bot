bc49b5863253f882cdc770bc5475cc54f5972337
twobiers <22715034+twobiers@users.noreply.github.com>
2024-07-23 21:19:51 +0200

    use ghcr image reference and don't build image

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,5 @@ set -xeuo pipefail
 
 cd /home/csc
 
-export DOCKER_BUILDKIT=1
-docker build --pull . -t csz-bot:latest
-docker compose up -d --remove-orphans
+docker compose up --pull -d --remove-orphans
 docker image prune -a -f
