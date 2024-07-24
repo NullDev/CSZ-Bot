df26088acba3fa7ec6ba9569785923d05d3dfe2c
twobiers <22715034+twobiers@users.noreply.github.com>
2024-07-24 20:28:05 +0200

    create script to deploy ephemeral env

---
+++ .infra/deploy-ephemeral.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+if [[ -z "${BOT_HOME_PATH}" ]]; then
+    echo "BOT_HOME_PATH needs to be set"
+    exit 1
+fi
+
+if [[ -z "${EPHEMERAL_BOT_CONFIG}" ]]; then
+    echo "EPHEMERAL_BOT_CONFIG needs to be set"
+    exit 1
+fi
+
+TEMP_DIR=$(mktemp -d)
+cd "$TEMP_DIR"
+
+rsync -av \
+    --exclude='logs' \
+    --exclude='storage.db' \
+    --exclude='config.json' \
+    "$BOT_HOME_PATH" "$TEMP_DIR"
+
+echo "$EPHEMERAL_BOT_CONFIG" >config.json
+
+docker compose up \
+    -f compose.ephemeral.yaml \
+    --pull \
+    -d \
+    --remove-orphans
