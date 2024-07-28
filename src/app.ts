6049dd372aa667874d6bf3717aff1b406e986e1c
holzmaster <holzmaster@outlook.com>
2024-07-29 01:34:51 +0200

    Actually use semver and build-id as pre-release ID, so we get proper ordering

--- src/app.ts
+++ src/app.ts
@@ -32,8 +32,8 @@ import * as cronService from "@/service/cron.js";
 const env = process.env;
 
 const commitHash = env.RELEASE_IDENTIFIER || undefined; // || instead of ?? to make empty string undefined
-const buildMetadata = env.BUILD_NUMBER ? `+build.${env.BUILD_NUMBER}` : "";
-const release = commitHash ? `csz-bot@${commitHash}${buildMetadata}` : undefined;
+const buildMetadata = env.BUILD_NUMBER ? `-build.${env.BUILD_NUMBER}` : "";
+const release = commitHash ? `csz-bot@0.0.0${buildMetadata}+${commitHash}` : undefined;
 
 {
     const prodMode =
