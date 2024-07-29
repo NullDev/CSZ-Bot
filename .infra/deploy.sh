fd1af6e536ca31fbba02ec2aae90ee80b57a8873
twobiers <22715034+twobiers@users.noreply.github.com>
2024-07-29 14:41:18 +0200

    do not change directory

--- .infra/deploy.sh
+++ .infra/deploy.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-if [[ -z "${WORKING_DIRECTORY}" ]]; then
-    cd "${WORKING_DIRECTORY}"
-fi
-
 docker compose pull bot
 docker compose up -d --remove-orphans
 docker image prune -a -f
