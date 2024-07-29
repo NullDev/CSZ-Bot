68dbec582daf9ff396cf2d3ccf01a3dc3af9100a
twobiers <22715034+twobiers@users.noreply.github.com>
2024-07-29 14:56:15 +0200

    pass in home path

--- .infra/deploy.sh
+++ .infra/deploy.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-if [[ -z "${WORKING_DIRECTORY}" ]]; then
-    cd "${WORKING_DIRECTORY}"
-fi
-
-docker compose pull bot
-docker compose up -d --remove-orphans
+docker compose -f "$BOT_HOME_DIR/.infra/compose.yaml" pull bot
+docker compose -f "$BOT_HOME_DIR/.infra/compose.yaml" up -d --remove-orphans
 docker image prune -a -f
