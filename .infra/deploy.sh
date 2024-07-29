48ee305eda16e7ac2a098fa898adfa1e23bc4200
twobiers <22715034+twobiers@users.noreply.github.com>
2024-07-29 14:49:04 +0200

    Revert "do not change directory"
    This reverts commit fd1af6e536ca31fbba02ec2aae90ee80b57a8873.

--- .infra/deploy.sh
+++ .infra/deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+if [[ -z "${WORKING_DIRECTORY}" ]]; then
+    cd "${WORKING_DIRECTORY}"
+fi
+
 docker compose pull bot
 docker compose up -d --remove-orphans
 docker image prune -a -f
