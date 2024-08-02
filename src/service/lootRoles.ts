baf089c66b4acc37374ca292ac53cd7e4f3ba497
holzmaster <15527285+holzmaster@users.noreply.github.com>
2024-08-02 02:16:55 +0200

    Update lootRoles.ts

--- src/service/lootRoles.ts
+++ src/service/lootRoles.ts
@@ -23,7 +23,7 @@ export async function startAsseGuardShift(
         embeds: [
             {
                 title: "Schichtbeginn",
-                description: `Die Wärterschicht von ${member} am Eingang des Atommüllendlagers hat begonnen.\n\nGegen etwas Süßes lässt ${member} vielleicht ein Fass in die Grube werfen.`,
+                description: `Die Wärterschicht von ${member} am Eingang des Atommüllendlagers hat begonnen.\n\nGegen etwas Süßes lässt ${member} vielleicht ein Fass in der Grube verschwinden.`,
                 color: 0x00ff00,
             },
         ],
