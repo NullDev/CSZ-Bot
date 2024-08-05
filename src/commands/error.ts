37cfc5f6d2423a2edacea25fef76579b87c5d2dc
Fionera <fionera@fionera.de>
2024-08-06 01:15:15 +0200

    revert feler

--- src/commands/error.ts
+++ src/commands/error.ts
@@ -1,14 +1,10 @@
 import type { MessageCommand } from "./command.js";
-import { getTrichterUnserEmbed } from "@/service/trichterUnser.js";
-import type { BotContext } from "@/context.js";
-import type { ProcessableMessage } from "@/service/command.js";
 
 export default class ErrorCommand implements MessageCommand {
     name = "feler";
     description = "Macht ne exception. Mehr nicht.";
 
-    async handleMessage(_: ProcessableMessage, ctx: BotContext) {
-        // throw new Error("Fehler!");
-        await getTrichterUnserEmbed(ctx);
+    async handleMessage() {
+        throw new Error("Fehler!");
     }
 }
