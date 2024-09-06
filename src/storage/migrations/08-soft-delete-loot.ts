de9d703fc46a837e1ebd7d6f115bf311e57029ce
holzmaster <holzmaster@outlook.com>
2024-09-06 17:44:45 +0200

    Remove kebab migration because it will happen automatically

--- src/storage/migrations/08-soft-delete-loot.ts
+++ src/storage/migrations/08-soft-delete-loot.ts
@@ -13,47 +13,6 @@ export async function up(db: Kysely<any>) {
 
     await db.schema.alterTable("loot").dropColumn("validUntil").execute();
     await db.deleteFrom("loot").where("winnerId", "is", null).execute();
-
-    // IDs at the time of migration
-    enum LootTypeId {
-        DOENER = 4,
-        VERSCHIMMELTER_DOENER = 32,
-    }
-
-    const now = Date.now();
-    const maxKebabAge = 3 * 24 * 60 * 60 * 1000;
-    const kebabs = await db
-        .selectFrom("loot")
-        .where("lootKindId", "=", LootTypeId.DOENER)
-        .selectAll()
-        .execute();
-
-    for (const k of kebabs) {
-        const itemAge = now - new Date(k.claimedAt).getTime();
-        if (itemAge <= maxKebabAge) {
-            continue;
-        }
-
-        await db
-            .updateTable("loot")
-            .where("id", "=", k.id)
-            .set({ deletedAt: sql`current_timestamp` })
-            .execute();
-
-        await db
-            .insertInto("loot")
-            .values({
-                ...k,
-                id: undefined,
-                predecessor: k.id,
-                lootKindId: LootTypeId.VERSCHIMMELTER_DOENER,
-                displayName: "Verschimmelter Döner",
-                description: "Du hättest ihn früher essen sollen",
-                usedImage: null,
-            })
-            .returning(["id"])
-            .executeTakeFirstOrThrow();
-    }
 }
 
 export async function down(_db: Kysely<any>) {
