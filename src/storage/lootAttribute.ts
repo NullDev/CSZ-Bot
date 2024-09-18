776870883fbad5a5cec9ce9202fb6e54e8e271ea
holzmaster <holzmaster@outlook.com>
2024-09-18 23:34:25 +0200

    Remove unused file

--- src/storage/lootAttribute.ts
+++
@@ -1,7 +0,0 @@
-import type { GuildChannel, GuildMember, Message, TextChannel, User } from "discord.js";
-import { type ExpressionBuilder, sql } from "kysely";
-
-import type { BotContext } from "@/context.js";
-import type { Database, Loot, LootId, LootInsertable, LootOrigin, LootTable } from "./db/model.js";
-
-import db from "@db";
