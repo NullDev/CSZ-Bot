f5bc92cbcea01bf022a4d9aba55642e9cadd619b
holzmaster <15527285+holzmaster@users.noreply.github.com>
2024-09-20 05:19:48 +0200

    Update gegenstand.ts

--- src/commands/gegenstand.ts
+++ src/commands/gegenstand.ts
@@ -326,7 +326,7 @@ export default class GegenstandCommand implements ApplicationCommand {
             const emote = lootDataService.getEmote(interaction.guild, item);
             completions.push({
                 // auto completions don't support discord emotes, only unicode ones
-                name: typeof emote === "string" ? `${emote} ${item.displayName}` : item.displayName,
+                name: typeof emote === "string" ? `${emote} ${item.displayName} (${item.id})` : item.displayName + " (" + item.id + ")",
                 value: String(item.id),
             });
         }
