53df38fce4e5e7cf8d4db66cba16d2cbb7633030
holzmaster <holzmaster@outlook.com>
2024-10-08 17:37:38 +0200

    Adjust rates

--- src/service/lootData.ts
+++ src/service/lootData.ts
@@ -52,7 +52,7 @@ export enum LootKindId {
 export const lootTemplates: LootTemplate[] = [
     {
         id: LootKindId.NICHTS,
-        weight: 35,
+        weight: 32,
         displayName: "Nichts",
         titleText: "✨Nichts✨",
         dropDescription: "¯\\_(ツ)_/¯",
@@ -378,7 +378,7 @@ export const lootTemplates: LootTemplate[] = [
     },
     {
         id: LootKindId.SCHICHTBEGINN_ASSE_2,
-        weight: 9,
+        weight: 12,
         displayName: "Wärter Asse II",
         titleText: "Den Schichtbeginn in der Asse II",
         dropDescription: "Deine Wärterschicht bei der Asse II beginnt!",
