2a664f37f1d35961c858041a141b1914d7315086
holzmaster <holzmaster@outlook.com>
2024-10-09 17:08:45 +0200

    Fix remaining embeds

--- src/service/lootDrop.ts
+++ src/service/lootDrop.ts
@@ -146,6 +146,8 @@ export async function postLootDrop(
     if (donor !== undefined && interaction.user.id === donor.id) {
         await message.edit({
             content: `${interaction.user} hat versucht, das Geschenki selbst zu öffnen. Das geht aber nichti ${hamster}\nDas Geschenk macht plopp und ist weg! 🎈`,
+            embeds: [],
+            components: [],
         });
         return;
     }
