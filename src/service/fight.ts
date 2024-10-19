978169516aacc88c95e3cc0cf87727847fdf38f5
Feluin <pascal.roecker.1997@gmail.com>
2024-10-19 19:29:08 +0200

    added rudamentary visualisation

--- src/service/fight.ts
+++ src/service/fight.ts
@@ -1,8 +1,8 @@
-import {APIEmbed, APIEmbedField, BooleanCache, CacheType, InteractionResponse} from "discord.js";
-import {JSONEncodable} from "@discordjs/util";
-import {setTimeout} from "node:timers/promises";
-import {name} from "croner";
-import {en} from "chrono-node";
+import { APIEmbed, APIEmbedField, BooleanCache, CacheType, InteractionResponse } from "discord.js";
+import { JSONEncodable } from "@discordjs/util";
+import { setTimeout } from "node:timers/promises";
+import { name } from "croner";
+import { en } from "chrono-node";
 
 interface EquipableWeapon {
     attack: Range;
@@ -29,10 +29,9 @@ export interface BaseEntity {
 
 function calcDamage(rawDamage: number, defence: number) {
     if (defence >= rawDamage) {
-        return {rawDamage: rawDamage, damage: 0, mitigated: rawDamage};
+        return { rawDamage: rawDamage, damage: 0, mitigated: rawDamage };
     }
-    return {rawDamage: rawDamage, damage: rawDamage - defence, mitigated: defence};
-
+    return { rawDamage: rawDamage, damage: rawDamage - defence, mitigated: defence };
 }
 
 export class Entity {
@@ -56,14 +55,24 @@ export class Entity {
         if (this.stats.weapon) {
             rawDamage += randomValue(this.stats.weapon.attack);
         }
-        this.stats.items.filter(value => value.attackModifier).forEach(value =>
-            rawDamage += randomValue(value.attackModifier!)
-        );
+        this.stats.items
+            .filter(value => value.attackModifier)
+            .forEach(value => (rawDamage += randomValue(value.attackModifier!)));
         let defence = enemy.defend();
         let result = calcDamage(rawDamage, defence);
-        console.log(this.stats.name + " (" + this.stats.health + ") hits " +
-            enemy.stats.name + " (" + enemy.stats.health + ") for "
-            + result.damage + " mitigated " + result.mitigated);
+        console.log(
+            this.stats.name +
+                " (" +
+                this.stats.health +
+                ") hits " +
+                enemy.stats.name +
+                " (" +
+                enemy.stats.health +
+                ") for " +
+                result.damage +
+                " mitigated " +
+                result.mitigated,
+        );
         enemy.stats.health -= result.damage;
         this.lastattack = result.rawDamage;
         return result;
@@ -74,13 +83,12 @@ export class Entity {
         if (this.stats.armor) {
             defence += randomValue(this.stats.armor.defence);
         }
-        this.stats.items.filter(value => value.defenceModifier).forEach(value =>
-            defence += randomValue(value.defenceModifier!)
-        );
+        this.stats.items
+            .filter(value => value.defenceModifier)
+            .forEach(value => (defence += randomValue(value.defenceModifier!)));
         this.lastdefence = defence;
         return defence;
     }
-
 }
 
 interface Range {
@@ -96,26 +104,30 @@ interface EquipableItem {
     modifyAttack?: (scene: FightScene) => void;
 }
 
-
 export interface FightScene {
     player: Entity;
     enemy: Entity;
 }
 
-let exampleWeapon: EquipableWeapon = {name: "dildo", attack: {min: 3, max: 8}};
-let exampleArmor: EquipableArmor = {name: "nachthemd", defence: {min: 2, max: 5}, health: 20};
+let exampleWeapon: EquipableWeapon = { name: "dildo", attack: { min: 3, max: 8 } };
+let exampleArmor: EquipableArmor = { name: "nachthemd", defence: { min: 2, max: 5 }, health: 20 };
 let healitem: EquipableItem = {
     name: "powerade",
     afterFight: scene1 => {
         if (Math.random() < 0.2) {
-            let health = randomValue({min: 1, max: 5});
+            let health = randomValue({ min: 1, max: 5 });
             scene1.player.stats.health += health;
             scene1.player.itemtext.push(healitem!.name + " +" + health + "HP");
-            console.log(scene1.player.stats.name + " gets healed by " + health + " from item" + healitem.name);
+            console.log(
+                scene1.player.stats.name +
+                    " gets healed by " +
+                    health +
+                    " from item" +
+                    healitem.name,
+            );
         }
-    }
+    },
 };
-
 
 type result = "PLAYER" | "ENEMY" | undefined;
 
@@ -133,24 +145,29 @@ function randomValue(range: Range) {
 }
 
 export async function fight(interactionResponse: InteractionResponse<BooleanCache<CacheType>>) {
-    let playerstats = {name: "Player", health: 80, baseDamage: 1, baseDefence: 0, items: [], weapon: exampleWeapon};
+    let playerstats = {
+        name: "Player",
+        health: 80,
+        baseDamage: 1,
+        baseDefence: 0,
+        items: [],
+        weapon: exampleWeapon,
+    };
     let enemystats = {
         name: "Gudrun die Hexe",
         health: 120,
         baseDamage: 1,
         baseDefence: 1,
         items: [healitem],
-        armor: exampleArmor
+        armor: exampleArmor,
     };
 
     let enemy = new Entity(enemystats);
     let player = new Entity(playerstats);
 
-    let scene
-        :
-        FightScene = {
+    let scene: FightScene = {
         player: player,
-        enemy: enemy
+        enemy: enemy,
     };
     while (checkWin(scene) === undefined) {
         player.itemtext = [];
@@ -171,42 +188,42 @@ export async function fight(interactionResponse: InteractionResponse<BooleanCach
             if (!value.afterFight) {
                 return;
             }
-            value.afterFight({player: enemy, enemy: player});
+            value.afterFight({ player: enemy, enemy: player });
         });
-        await interactionResponse.edit({embeds: [renderFightEmbedded(scene)]});
+        await interactionResponse.edit({ embeds: [renderFightEmbedded(scene)] });
         await setTimeout(200);
     }
-
 }
 
 function renderStats(player: Entity) {
     while (player.itemtext.length < 5) {
-        player.itemtext.push("-")
+        player.itemtext.push("-");
     }
 
     return {
         name: player.stats.name,
-        value:
-            `❤️HP${player.stats.health}/${player.maxhealth}
-            ❤️${"=".repeat(Math.max(0, player.stats.health / player.maxhealth * 10))}
+        value: `❤️HP${player.stats.health}/${player.maxhealth}
+            ❤️${"=".repeat(Math.max(0, (player.stats.health / player.maxhealth) * 10))}
             ⚔️Waffe: ${player.stats.weapon?.name ?? "Schwengel"} ${player.lastattack}
             🛡️Rüstung: ${player.stats.armor?.name ?? "Nackt"} ${player.lastdefence}
             📚Items:
             ${player.itemtext.join("\n")}
         `,
-        inline: true
+        inline: true,
     };
-
 }
 
 function renderFightEmbedded(fightscene: FightScene): JSONEncodable<APIEmbed> | APIEmbed {
     return {
         title: `Kampf zwischen ${fightscene.player.stats.name} und ${fightscene.enemy.stats.name}`,
         description: "Lol hier beschreibung",
-        fields: [renderStats(fightscene.player), renderStats(fightscene.enemy), {
-            name: "Verlauf",
-            value: " "
-        }]
+        fields: [
+            renderStats(fightscene.player),
+            renderStats(fightscene.enemy),
+            {
+                name: "Verlauf",
+                value: " ",
+            },
+        ],
     };
 }
-
