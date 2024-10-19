f18a96ffce82a65fd07cc6902ba62feea1b0b8ac
Feluin <pascal.roecker.1997@gmail.com>
2024-10-19 19:29:08 +0200

    added rudamentary visualisation

--- src/commands/fight.ts
+++ src/commands/fight.ts
@@ -1,30 +1,25 @@
-import type {ApplicationCommand} from "@/commands/command.js";
+import type { ApplicationCommand } from "@/commands/command.js";
 import {
-    APIEmbed, APIEmbedField,
+    APIEmbed,
+    APIEmbedField,
     CommandInteraction,
     ContextMenuCommandBuilder,
     SlashCommandBuilder,
-    SlashCommandUserOption
+    SlashCommandUserOption,
 } from "discord.js";
-import {BotContext} from "@/context.js";
-import {Entity, fight, FightScene} from "@/service/fight.js";
-import {JSONEncodable} from "@discordjs/util";
-
+import { BotContext } from "@/context.js";
+import { Entity, fight, FightScene } from "@/service/fight.js";
+import { JSONEncodable } from "@discordjs/util";
 
 export default class FightCommand implements ApplicationCommand {
-
     readonly description = "TBD";
     readonly name = "fight";
     readonly applicationCommand = new SlashCommandBuilder()
         .setName(this.name)
-        .setDescription(this.description)
-    ;
+        .setDescription(this.description);
 
     async handleInteraction(command: CommandInteraction, context: BotContext) {
-
         let interactionResponse = await command.deferReply();
         fight(interactionResponse);
     }
 }
-
-
