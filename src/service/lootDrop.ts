8a252cd8e426236997d58a5d469ba473835c8cb9
twobiers <22715034+twobiers@users.noreply.github.com>
2024-11-04 19:03:49 +0100

    remove rarity from title

--- src/service/lootDrop.ts
+++ src/service/lootDrop.ts
@@ -197,7 +197,7 @@ export async function postLootDrop(
     await message.edit({
         embeds: [
             {
-                title: `Das Geschenk enthielt: ${template.titleText} ${initialAttribute?.shortDisplay ?? ""}`.trim(),
+                title: `Das Geschenk enthielt: ${template.titleText}`,
                 description: template.dropDescription,
                 image: attachment
                     ? {
