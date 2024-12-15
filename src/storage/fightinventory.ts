7423034af88ca8c2f507a87a49dfa86806a8be75
Feluin <pascal.roecker.1997@gmail.com>
2024-12-15 12:51:11 +0100

    added fight with current inventory

--- src/storage/fightinventory.ts
+++ src/storage/fightinventory.ts
@@ -1,10 +1,16 @@
-import type {User} from "discord.js";
+import type { User } from "discord.js";
 import db from "@db";
-import {Equipable, EquipableArmor, EquipableItem, EquipableWeapon, FightItemType} from "@/service/fightData.js";
-import type {LootId} from "@/storage/db/model.js";
+import {
+    Equipable,
+    EquipableArmor,
+    EquipableItem,
+    EquipableWeapon,
+    FightItemType,
+} from "@/service/fightData.js";
+import type { LootId } from "@/storage/db/model.js";
 import * as lootDataService from "@/service/lootData.js";
 import * as lootService from "@/service/loot.js";
-import {LootKindId, resolveLootTemplate} from "@/service/lootData.js";
+import { LootKindId, resolveLootTemplate } from "@/service/lootData.js";
 
 export async function getFightInventoryUnsorted(userId: User["id"], ctx = db()) {
     return await ctx
@@ -19,24 +25,30 @@ export async function getFightInventoryEnriched(userId: User["id"], ctx = db()) 
     const enriched = [];
     for (const equip of unsorted) {
         enriched.push({
-            gameTemplate: await getGameTemplate((await lootService.getUserLootById(userId, equip.lootId, ctx))?.lootKindId),
-            ...equip
+            gameTemplate: await getGameTemplate(
+                (await lootService.getUserLootById(userId, equip.lootId, ctx))?.lootKindId,
+            ),
+            ...equip,
         });
     }
     return {
-        weapon: enriched.filter(value => value.gameTemplate?.type === "weapon")
-            .map(value =>
-            value.gameTemplate as EquipableWeapon).shift(),
-        armor: enriched.filter(value => value.gameTemplate?.type === "armor")
-            .map(value =>
-            value.gameTemplate as EquipableArmor).shift(),
-        items: enriched.filter(value => value.gameTemplate?.type === "item")
-            .map(value =>
-            value.gameTemplate as EquipableItem)
+        weapon: enriched
+            .filter(value => value.gameTemplate?.type === "weapon")
+            .map(value => value.gameTemplate as EquipableWeapon)
+            .shift(),
+        armor: enriched
+            .filter(value => value.gameTemplate?.type === "armor")
+            .map(value => value.gameTemplate as EquipableArmor)
+            .shift(),
+        items: enriched
+            .filter(value => value.gameTemplate?.type === "item")
+            .map(value => value.gameTemplate as EquipableItem),
     };
 }
 
-export async function getGameTemplate(lootKindId: LootKindId | undefined): Promise<Equipable | undefined> {
+export async function getGameTemplate(
+    lootKindId: LootKindId | undefined,
+): Promise<Equipable | undefined> {
     return lootKindId ? resolveLootTemplate(lootKindId)?.gameEquip : undefined;
 }
 
@@ -56,7 +68,7 @@ export async function equipItembyLoot(userId: User["id"], lootId: LootId, ctx = 
     const maxItems = {
         weapon: 1,
         armor: 1,
-        item: 3
+        item: 3,
     };
 
     const unequippeditems: string[] = [];
@@ -66,7 +78,7 @@ export async function equipItembyLoot(userId: User["id"], lootId: LootId, ctx = 
             const unequipitem = await lootService.getUserLootById(
                 userId,
                 equippedStuff[i].lootId,
-                ctx
+                ctx,
             );
             unequippeditems.push(unequipitem?.displayName ?? String(equippedStuff[i].lootId));
             await ctx.deleteFrom("fightinventory").where("id", "=", equippedStuff[i].id).execute();
@@ -77,9 +89,9 @@ export async function equipItembyLoot(userId: User["id"], lootId: LootId, ctx = 
             .values({
                 userid: userId,
                 lootId: lootId,
-                equippedSlot: type
+                equippedSlot: type,
             })
             .execute();
-        return {unequipped: unequippeditems, equipped: item};
+        return { unequipped: unequippeditems, equipped: item };
     });
 }
