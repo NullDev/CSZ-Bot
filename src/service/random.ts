6882cd7b97e9725de1f3c825e452c05b18fd77bf
holzmaster <holzmaster@outlook.com>
2025-01-23 14:58:23 +0100

    Make random API more clear

--- src/service/random.ts
+++ src/service/random.ts
@@ -1,8 +1,25 @@
-export interface Range {
+export type Range = ExclusiveRange | InclusiveRange;
+
+/**
+ * [min, ceil(maxExclusive))
+ */
+export interface ExclusiveRange {
     min: number;
-    max: number;
+    maxExclusive: number;
 }
 
+/**
+ * [min, ceil(maxInclusive)]
+ */
+export interface InclusiveRange {
+    min: number;
+    maxInclusive: number;
+}
+
+/**
+ * Based on https://stackoverflow.com/a/24152886
+ */
 export function randomValue(range: Range): number {
-    return Math.round(range.min + Math.random() * (range.max - range.min));
+    const upperLimit = "maxInclusive" in range ? range.maxInclusive + 1 : range.maxExclusive;
+    return Math.round(range.min + Math.random() * (upperLimit - range.min));
 }
