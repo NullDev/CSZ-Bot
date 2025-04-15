5c0229dd8571215d7e4e2807a528280bb4e3304a
twobiers <22715034+twobiers@users.noreply.github.com>
2025-04-15 21:16:18 +0200

    Update imageUrl on conflict

--- src/storage/lauscher.ts
+++ src/storage/lauscher.ts
@@ -67,7 +67,12 @@ export async function insertTrackMetadata(track: Track, artists: Artist[], ctx =
             durationInMs: track.duration_ms,
             imageUrl: track.album.images[0]?.url ?? null,
         })
-        .onConflict(oc => oc.column("trackId").doUpdateSet({ durationInMs: track.duration_ms }))
+        .onConflict(oc =>
+            oc.column("trackId").doUpdateSet({
+                durationInMs: track.duration_ms,
+                imageUrl: track.album.images[0]?.url ?? null,
+            }),
+        )
         .executeTakeFirstOrThrow();
 
     await ctx
@@ -99,6 +104,7 @@ export async function trackMetadataExists(track: Track, ctx = db()): Promise<boo
         .selectFrom("spotifyTracks")
         .where("trackId", "=", track.id)
         .where("durationInMs", "=", track.duration_ms)
+        .where("imageUrl", "=", track.album.images[0]?.url ?? null)
         .limit(1)
         .selectAll()
         .executeTakeFirst();
