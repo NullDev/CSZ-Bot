fe0819f299b585f97547827b199f6586556cd089
twobiers <22715034+twobiers@users.noreply.github.com>
2025-04-15 21:19:22 +0200

    format code

--- src/commands/lauscher.ts
+++ src/commands/lauscher.ts
@@ -81,7 +81,10 @@ async function drawTrackToplistCanvas(user: User, tracks: TrackStat[]): Promise<
         ctx.fillText(trackStr, 160, 80 + i * 100);
         ctx.textAlign = "right";
         ctx.fillText(`${track.count}x`, 1000, 55 + i * 100);
-        const imageUrl = track.imageUrl && track.imageUrl.trim() !== "" ? track.imageUrl : "assets/images/fallback.png";
+        const imageUrl =
+            track.imageUrl && track.imageUrl.trim() !== ""
+                ? track.imageUrl
+                : "assets/images/fallback.png";
         ctx.drawImage(await loadImage(imageUrl), 80, 15 + i * 100, 64, 64);
     }
 
