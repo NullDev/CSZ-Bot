436fb40eb4b77c206c3b40209a127f9ed9d5397b
holzmaster <holzmaster@outlook.com>
2025-04-19 16:46:55 +0200

    Add getOrInsert/getOrInsertComputed polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,7 +1,7 @@
-// type polyfill for Math.sumPrecise (currently in stage 2):
-// https://github.com/tc39/proposal-math-sum
-// Remove this as soon as it is stable
 declare global {
+    // type polyfill for Math.sumPrecise (currently in stage 2):
+    // https://github.com/tc39/proposal-math-sum
+    // Remove this as soon as it is stable
     interface Math {
         sumPrecise: (values: number[]) => number;
     }
@@ -19,6 +19,12 @@ declare global {
         forEach(fn: (value: T) => unknown): void;
     }
     */
+
+    // https://github.com/tc39/proposal-upsert
+    interface Map<K, V> {
+        getOrInsert(key: K, defaultValue: V): V;
+        getOrInsertComputed<TK extends K>(key: TK, callbackFunction: (key: TK) => V): V;
+    }
 }
 
 if (typeof Math.sumPrecise !== "function") {
@@ -29,4 +35,22 @@ if (typeof Math.sumPrecise !== "function") {
 // @ts-ignore: Property "UrlPattern" does not exist
 if (!globalThis.URLPattern) {
     await import("urlpattern-polyfill");
+}
+
+// https://github.com/tc39/proposal-upsert
+if (typeof Map.prototype.getOrInsert === "undefined") {
+    Map.prototype.getOrInsert = function (key, defaultValue) {
+        if (!this.has(key)) {
+            this.set(key, defaultValue);
+        }
+        return this.get(key);
+    };
+}
+if (typeof Map.prototype.getOrInsertComputed === "undefined") {
+    Map.prototype.getOrInsertComputed = function (key, callbackFunction) {
+        if (!this.has(key)) {
+            this.set(key, callbackFunction(key));
+        }
+        return this.get(key);
+    };
 }
