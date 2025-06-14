a81e8b49cc98ae2471c50abf1c2c77422e9ada02
holzmaster <holzmaster@outlook.com>
2025-06-14 20:26:33 +0200

    Add debugging command

--- src/commands/error.ts
+++ src/commands/error.ts
@@ -1,10 +1,13 @@
 import type { MessageCommand } from "./command.js";
+import type { ProcessableMessage } from "@/service/command.js";
+import type { BotContext } from "@/context.js";
 
 export default class ErrorCommand implements MessageCommand {
     name = "feler";
     description = "Macht ne exception. Mehr nicht.";
 
-    async handleMessage() {
-        throw new Error("Fehler!");
+    async handleMessage(_: ProcessableMessage, context: BotContext): Promise<void> {
+        (await import("../service/lootDegradation.js")).runHalfLife(context);
+        // throw new Error("Fehler!");
     }
 }
