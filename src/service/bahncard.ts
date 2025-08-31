a9c21585cef9d892f287f39ccee0d250a576c1e2
holzmaster <holzmaster@outlook.com>
2025-09-01 01:23:33 +0200

    Add fonts

--- src/service/bahncard.ts
+++ src/service/bahncard.ts
@@ -1,7 +1,7 @@
 import * as fs from "node:fs/promises";
 
 import type { User } from "discord.js";
-import { createCanvas, loadImage } from "@napi-rs/canvas";
+import { createCanvas, GlobalFonts, loadImage } from "@napi-rs/canvas";
 
 import type { BotContext } from "@/context.js";
 import type { Loot } from "@/storage/db/model.js";
@@ -10,6 +10,13 @@ import type { LootTemplate } from "src/storage/loot.js";
 const namePos = { x: 38, y: 567 };
 const avatarPos = { x: 775, y: 221 };
 const avatarMaxSize = { width: 160, height: 160 };
+
+GlobalFonts.registerFromPath("assets/fonts/DBNeoScreenSans-Black.woff2", "DBNeoScreenSans Black");
+GlobalFonts.registerFromPath("assets/fonts/DBNeoScreenSans-Bold.woff2", "DBNeoScreenSans Bold");
+GlobalFonts.registerFromPath(
+    "assets/fonts/DBNeoScreenSans-Regular.woff2",
+    "DBNeoScreenSans Regular",
+);
 
 export async function drawBahncardImage(
     _context: BotContext,
