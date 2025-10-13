ca36e71d8083b584df3e496ed6de3e205d5de1b3
holzmaster <holzmaster@outlook.com>
2025-10-13 22:49:38 +0200

    Fix linter

--- src/service/location.ts
+++ src/service/location.ts
@@ -1,13 +1,13 @@
-import type {User} from "discord.js";
+import type { User } from "discord.js";
 
 import assertNever from "@/utils/assertNever.js";
 
 import * as locationHistory from "@/storage/locationHistory.js";
-import type {MapLocation} from "@/storage/db/model.js";
+import type { MapLocation } from "@/storage/db/model.js";
 import fs from "node:fs/promises";
 import path from "node:path";
 
-export const startPosition = {x: 0, y: 0};
+export const startPosition = { x: 0, y: 0 };
 
 export type Direction = "NW" | "N" | "NE" | "W" | "X" | "E" | "SW" | "S" | "SE";
 
@@ -33,23 +33,23 @@ export async function move(user: User, direction: Direction) {
 function deriveNewPosition(position: Position, direction: Direction) {
     switch (direction) {
         case "NW":
-            return {x: position.x - 1, y: position.y - 1};
+            return { x: position.x - 1, y: position.y - 1 };
         case "N":
-            return {x: position.x, y: position.y - 1};
+            return { x: position.x, y: position.y - 1 };
         case "NE":
-            return {x: position.x + 1, y: position.y - 1};
+            return { x: position.x + 1, y: position.y - 1 };
         case "W":
-            return {x: position.x - 1, y: position.y};
+            return { x: position.x - 1, y: position.y };
         case "X":
-            return {x: position.x, y: position.y};
+            return { x: position.x, y: position.y };
         case "E":
-            return {x: position.x + 1, y: position.y};
+            return { x: position.x + 1, y: position.y };
         case "SW":
-            return {x: position.x - 1, y: position.y + 1};
+            return { x: position.x - 1, y: position.y + 1 };
         case "S":
-            return {x: position.x, y: position.y + 1};
+            return { x: position.x, y: position.y + 1 };
         case "SE":
-            return {x: position.x + 1, y: position.y + 1};
+            return { x: position.x + 1, y: position.y + 1 };
         default:
             assertNever(direction);
     }
@@ -76,25 +76,28 @@ export function canMove(position: Position, maxSize: Position, direction: Direct
 }
 
 interface Place {
-    "name": string,
-    "description": string,
-    "x": number,
-    "y": number,
-    "width": number,
-    "height": number,
-    "color": string
+    name: string;
+    description: string;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    color: string;
 }
 
 export async function getCurrentPlaceforUser(position: Position): Promise<Place | null> {
-    const locations: [Place] = JSON.parse(await fs.readFile(path.resolve("assets/maps/locations.json"), "utf-8"));
+    const locations: [Place] = JSON.parse(
+        await fs.readFile(path.resolve("assets/maps/locations.json"), "utf-8"),
+    );
     for (const location of locations) {
-        if (position.x > location.x && position.y > location.y
-            && position.x < location.x + location.width && position.y < location.y + location.height
+        if (
+            position.x > location.x &&
+            position.y > location.y &&
+            position.x < location.x + location.width &&
+            position.y < location.y + location.height
         ) {
             return location;
         }
     }
     return null;
-
-
 }
