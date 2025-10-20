69d448b06f985ec1caff7e4f78b1d7bb751dadcd
holzmaster <holzmaster@outlook.com>
2025-10-20 03:25:22 +0200

    Add migration

--- src/storage/migrations/21-polls-in-db.ts
+++ src/storage/migrations/21-polls-in-db.ts
@@ -1,6 +1,45 @@
 import { sql, type Kysely } from "kysely";
 
 export async function up(db: Kysely<any>) {
+    await db.schema
+        .createTable("polls")
+        .addColumn("id", "integer", c => c.primaryKey().autoIncrement())
+        .addColumn("guildId", "text", c => c.notNull())
+
+        .addColumn("sourceChannelId", "text", c => c.notNull())
+        .addColumn("sourceMessageId", "text", c => c.notNull())
+
+        .addColumn("embedChannelId", "text", c => c.notNull())
+        .addColumn("embedMessageId", "text", c => c.notNull())
+
+        .addColumn("authorId", "text", c => c.notNull())
+
+        .addColumn("question", "text", c => c.notNull())
+
+        .addColumn("multipleChoices", "boolean", c => c.notNull())
+        .addColumn("anonymous", "boolean", c => c.notNull())
+        .addColumn("extendable", "boolean", c => c.notNull())
+
+        .addColumn("endsAt", "timestamp", c => c)
+        .addColumn("ended", "boolean", c => c.notNull().defaultTo(false))
+
+        .addColumn("createdAt", "timestamp", c => c.notNull().defaultTo(sql`current_timestamp`))
+        .addColumn("updatedAt", "timestamp", c => c.notNull().defaultTo(sql`current_timestamp`))
+        .execute();
+
+    await db.schema
+        .createIndex("polls_endsAt")
+        .on("polls")
+        .columns(["endsAt asc"])
+        .where("endsAt", "is not", null)
+        .execute();
+
+    await db.schema
+        .createIndex("polls_embedMessageId")
+        .on("polls")
+        .column("embedMessageId")
+        .execute();
+
     await createUpdatedAtTrigger(db, "polls");
 }
 
