9708544c5e7c0194023b6f954e3b9d81f7186a77
holzmaster <holzmaster@outlook.com>
2025-10-22 14:30:57 +0200

    Handle deleted polls

--- src/handler/messageDeleteHandler.ts
+++ src/handler/messageDeleteHandler.ts
@@ -2,6 +2,10 @@ import type { ClientUser, Message } from "discord.js";
 
 import type { BotContext } from "@/context.js";
 
+import * as pollService from "@/service/poll.js";
+
+import log from "@log";
+
 const deleteInlineRepliesFromBot = (messageRef: Message<true>, botUser: ClientUser) =>
     Promise.allSettled(
         messageRef.channel.messages.cache
@@ -17,6 +21,8 @@ export default async function (message: Message<true>, context: BotContext) {
         return;
     }
 
+    await handlePollDeletion(message);
+
     const isNormalCommand =
         message.content.startsWith(context.prefix.command) ||
         message.content.startsWith(context.prefix.modCommand);
@@ -24,4 +30,14 @@ export default async function (message: Message<true>, context: BotContext) {
     if (isNormalCommand) {
         await deleteInlineRepliesFromBot(message, context.client.user);
     }
+}
+
+async function handlePollDeletion(message: Message<true>) {
+    const foundPoll = await pollService.findPollForEmbedMessage(message);
+    if (!foundPoll) {
+        return;
+    }
+
+    const deletedPoll = await pollService.deletePoll(foundPoll.id);
+    log.info(deletedPoll, "Poll deleted");
 }
