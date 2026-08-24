b49ce3065da300ca8125c17022ffcc9a702f442d
holzmaster <holzmaster@outlook.com>
2026-08-24 20:09:29 +0200

    Fix backup script

--- .infra/backup-db.sh
+++ .infra/backup-db.sh
@@ -5,5 +5,5 @@ TODAY=$(date +"%Y-%m-%d")
 mkdir -p backups
 TARGET_FILE=backups/$TODAY-backup.db
 
-sqlite3 storage.db ".backup $TARGET_FILE"
+sqlite3 /home/csc/data/storage.db ".backup $TARGET_FILE"
 zstd -19 --rm "$TARGET_FILE"
